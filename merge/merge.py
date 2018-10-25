b9139e3f7924608155c1b7038772c003a2088064
Chris Wilks <broadsword@gmail.com>
2018-10-24 20:37:41 -0400

    added gzip, higher level aggregation support

--- merge/merge.py
+++ merge/merge.py
@@ -1,6 +1,8 @@
 #!/bin/env python
 import sys
 import heapq
+import argparse
+import gzip
 
 CHRM_COL=0
 START_COL=1
@@ -9,21 +11,20 @@ COVERAGE_COL=4
 STRAND_COL=5
 SAMPLE_COL=6
 SAMPLES_COL=7
-LAST_COL=STRAND_COL+1
 #separate file columns
 FILE_SAMPLE_ID_COL=1
 
 
-def read_from_sources(fhs, filebuf, heap, current_chrm, files):
+def read_from_sources(fhs, filebuf, heap, current_chrm, files, last_col):
     #read 1 line from each source file looking for 1) same chromosome and 2) file EOF
     for (i,fin) in enumerate(fhs):
         fields = []
         filedone = False
         if filebuf[i][0] == '' and fin is not None:
-            fields = filebuf[i] = fin.readline().rstrip().split('\t')[:LAST_COL]
+            fields = filebuf[i] = fin.readline().rstrip().split('\t')[:last_col]
             #skip header
             if fields[0] == 'chrom':
-                fields = filebuf[i] = fin.readline().rstrip().split('\t')[:LAST_COL]
+                fields = filebuf[i] = fin.readline().rstrip().split('\t')[:last_col]
             filedone = filebuf[i][0] == ''
         elif filebuf[i][0] != '':
             fields = filebuf[i]
@@ -33,25 +34,34 @@ def read_from_sources(fhs, filebuf, heap, current_chrm, files):
             fields.append(files[i][FILE_SAMPLE_ID_COL])
             heapq.heappush(heap, (int(fields[START_COL]), fields))
             if fin is not None:
-                filebuf[i] = fin.readline().rstrip().split('\t')[:LAST_COL]
+                filebuf[i] = fin.readline().rstrip().split('\t')[:last_col]
                 filedone = filebuf[i][0] == ''
         if filedone: fhs[i] = None
     #if this is None, means we're at the end of all files
     return current_chrm
 
 
-def merge(sample_file_list):
+def merge(args):
+    last_col = STRAND_COL+1
+    samples_col = SAMPLES_COL
+    if args.append_samples:
+        last_col = STRAND_COL
+        samples_col = COVERAGE_COL
     files = []
-    with open(sample_file_list, "rb") as fin:
+    with open(args.list_file, "rb") as fin:
         files = [f.rstrip().split('\t') for f in list(fin)]
-    fhs = [open(f[0],"rb") for f in files]
+    fhs = []
+    if args.gzip:
+        fhs = [gzip.open(f[0],"rb") for f in files]
+    else:
+        fhs = [open(f[0],"rb") for f in files]
     n = len(fhs)
     filebuf = [['']]*n
     heap = []
     current_chrm = None
     previous = []
     while(True):
-        current_chrm = read_from_sources(fhs, filebuf, heap, current_chrm, files)
+        current_chrm = read_from_sources(fhs, filebuf, heap, current_chrm, files, last_col)
         #no more data, end
         if current_chrm is None: break
         #no more data for this chromosome, possibly no more data period, but do another read to find out
@@ -61,27 +71,32 @@ def merge(sample_file_list):
         current = heapq.heappop(heap)[1]
         #print current
         #format sampleID:coverage for Snaptron
-        current.append(",%s:%s" % (current[SAMPLE_COL],current[COVERAGE_COL]))
+        if not args.append_samples:
+            current.append(",%s:%s" % (current[SAMPLE_COL],current[COVERAGE_COL]))
         #check for previous junction in case we have to actually merge a junction from multiple sources
         if len(previous) > 0:
             #different junction, print the previous one
             if current[CHRM_COL] != previous[CHRM_COL] or current[START_COL] != previous[START_COL] or current[END_COL] != previous[END_COL]:
                 p = previous[:END_COL+1]
-                p.extend([previous[STRAND_COL],previous[SAMPLES_COL]])
+                p.extend([previous[STRAND_COL],previous[samples_col]])
                 sys.stdout.write("%s\n" % ("\t".join(p)))
                 previous = current
             #same junction add to the sample IDs/coverages
             else:
-                previous[SAMPLES_COL] += current[SAMPLES_COL]
+                previous[samples_col] += current[samples_col]
         else:
             previous = current
     #last one
     if len(previous) > 0:
         p = previous[:END_COL+1]
-        p.extend([previous[STRAND_COL],previous[SAMPLES_COL]])
+        p.extend([previous[STRAND_COL],previous[samples_col]])
         sys.stdout.write("%s\n" % ("\t".join(p)))
 
 if __name__ == '__main__':
-    sample_file_list = sys.argv[1]
-    merge(sample_file_list)
+    parser = argparse.ArgumentParser(description='junction merge')
+    parser.add_argument('--list-file', metavar='/path/to/file_with_queries', type=str, default=None, help='path to a file with list of junction files to aggregate and optionally their sample IDs (rail_ids) and study names)')
+    parser.add_argument('--gzip', action='store_const', const=True, default=False, help='input files are gzipped')
+    parser.add_argument('--append-samples', action='store_const', const=True, default=False, help='set this if aggregating beyond sample-level results')
+    args = parser.parse_args()
+    merge(args)
     
--- merge/merge.sh
+++ merge/merge.sh
@@ -1,14 +1,22 @@
 #!/bin/env bash
 set -o pipefail -o nounset -o errexit 
 
-directory=$1
-#filter out non-canonical splice junctions and sort by coordinates, while cutting out extraneous fields, and ? stranded junctions
-#assumes ${directory} is writable
-ls ${directory}/*.jx_bed | perl -ne 'chomp; $f=$_; `egrep -e "[AG]T-A[GC]"  $f | fgrep -v GT-AC | fgrep -v "?" | cut -f 1-7 | sort -k1,1 -k2,2n -k3,3n > $f.filtered.sorted`;'
-#TODO: replace this with a samples metadata sourced method to create the manifest file for the merge
-ls ?RR*.filtered.sorted | sort | perl -ne 'chomp; $f=$_; $f=~/_(.RP[^_]+)/; $SRP=$1; print "$f\t".$i++."\t$SRP\n";' > jx_sample_files
+top_directory=$1
 
-python merge.py sample_files > jx_sample_files.merged.tsv
-....multiple merge steps later.....
+for d in {`ls ${top_directory}/`};
+do
+    #filter out non-canonical splice junctions and sort by coordinates, while cutting out extraneous fields, and ? stranded junctions
+    #assumes ${directory} is writable
+    ls ${d}/*.jx_bed | perl -ne 'chomp; $f=$_; `egrep -e "[AG]T-A[GC]"  $f | fgrep -v GT-AC | fgrep -v "?" | cut -f 1-7 | sort -k1,1 -k2,2n -k3,3n > $f.filtered.sorted`;'
+    #TODO: replace this with a samples metadata sourced method to create the manifest file for the merge
+    ls ${d}/*.filtered.sorted | sort | perl -ne 'chomp; $f=$_; $f=~/_(.RP[^_]+)/; $SRP=$1; print "$f\t".$i++."\t$SRP\n";' > jx_sample_files
+    python merge.py --list-file sample_files --gzip | gzip > ${d}.jx_sample_files.merged.tsv.gz
+done
+
+#now do master merge
+ls *.jx_sample_files.merged.tsv.gz > all_jx_sample_files
+#TODO: change merge.py to handle samples at this level differently
+python merge.py --list-file all_jx_sample_files --gzip --append-samples > all.jx_sample_files.merged.tsv.gz
+
 #after all studies have been merged, generate per-study jx files (both coverage and BED) for recount
-zcat jx_sample_files.merged.tsv.gz | perl -ne 'BEGIN { open(IN,"<samples.tsv"); %h; %fhs; while($line=<IN>) { chomp($line); @f=split(/\t/,$line); $rid=$f[0]; $study=$f[4]; $h{$rid}=$study; if(!$fhs{$study}) { $fhs{$study}->[0]=IO::File->new("|gzip >$study.junction_coverage.tsv.gz"); $fhs{$study}->[1]=IO::File->new("|gzip >$study.junction_id_with_transcripts.bed.gz"); }} close(IN); $jx_id=-1; } chomp; $jx_id++; ($chrm,$start,$end,$strand,$samples_covs)=split(/\t/,$_); $samples_covs=~s/^,//; %studies_seen=(); for $sample (split(/,/,$samples_covs)) { ($sid,$c)=split(/:/,$sample); $study=$h{$sid}; ($covF,$bedF)=@{$fhs{$study}}; print $covF "$jx_id\t$sid\t$c\n"; if(!$studies_seen{$study}) { print $bedF "$chrm\t$start\t$end\t$jx_id|D:NA|A:NA|J:NA\t1000\t$strand\n"; $studies_seen{$study}=1; } }'
+zcat all.jx_sample_files.merged.tsv.gz | perl -ne 'BEGIN { open(IN,"<samples.tsv"); %h; %fhs; while($line=<IN>) { chomp($line); @f=split(/\t/,$line); $rid=$f[0]; $study=$f[4]; $h{$rid}=$study; if(!$fhs{$study}) { $fhs{$study}->[0]=IO::File->new("|gzip >$study.junction_coverage.tsv.gz"); $fhs{$study}->[1]=IO::File->new("|gzip >$study.junction_id_with_transcripts.bed.gz"); }} close(IN); $jx_id=-1; } chomp; $jx_id++; ($chrm,$start,$end,$strand,$samples_covs)=split(/\t/,$_); $samples_covs=~s/^,//; %studies_seen=(); for $sample (split(/,/,$samples_covs)) { ($sid,$c)=split(/:/,$sample); $study=$h{$sid}; ($covF,$bedF)=@{$fhs{$study}}; print $covF "$jx_id\t$sid\t$c\n"; if(!$studies_seen{$study}) { print $bedF "$chrm\t$start\t$end\t$jx_id|D:NA|A:NA|J:NA\t1000\t$strand\n"; $studies_seen{$study}=1; } }'
