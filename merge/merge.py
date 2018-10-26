4bc57bc6c13e00165722928bb774cf55172c6ab7
Chris Wilks <broadsword@gmail.com>
2018-10-26 18:33:46 -0400

    fixed higher level append mode

--- merge/merge.py
+++ merge/merge.py
@@ -15,7 +15,7 @@ SAMPLES_COL=7
 FILE_SAMPLE_ID_COL=1
 
 
-def read_from_sources(fhs, filebuf, heap, current_chrm, files, last_col):
+def read_from_sources(args, fhs, filebuf, heap, current_chrm, files, last_col):
     #read 1 line from each source file looking for 1) same chromosome and 2) file EOF
     for (i,fin) in enumerate(fhs):
         fields = []
@@ -31,7 +31,8 @@ def read_from_sources(fhs, filebuf, heap, current_chrm, files, last_col):
         if len(fields) > 0 and (current_chrm is None or fields[CHRM_COL] == current_chrm):
             current_chrm = fields[CHRM_COL]
             #make sure we trak the sample ID
-            fields.append(files[i][FILE_SAMPLE_ID_COL])
+            if not args.append_samples:
+                fields.append(files[i][FILE_SAMPLE_ID_COL])
             heapq.heappush(heap, (int(fields[START_COL]), fields))
             if fin is not None:
                 filebuf[i] = fin.readline().rstrip().split('\t')[:last_col]
@@ -42,9 +43,11 @@ def read_from_sources(fhs, filebuf, heap, current_chrm, files, last_col):
 
 
 def merge(args):
+    strand_col = STRAND_COL
     last_col = STRAND_COL+1
     samples_col = SAMPLES_COL
     if args.append_samples:
+        strand_col = strand_col - 2
         last_col = STRAND_COL
         samples_col = COVERAGE_COL
     files = []
@@ -61,7 +64,7 @@ def merge(args):
     current_chrm = None
     previous = []
     while(True):
-        current_chrm = read_from_sources(fhs, filebuf, heap, current_chrm, files, last_col)
+        current_chrm = read_from_sources(args, fhs, filebuf, heap, current_chrm, files, last_col)
         #no more data, end
         if current_chrm is None: break
         #no more data for this chromosome, possibly no more data period, but do another read to find out
@@ -78,7 +81,7 @@ def merge(args):
             #different junction, print the previous one
             if current[CHRM_COL] != previous[CHRM_COL] or current[START_COL] != previous[START_COL] or current[END_COL] != previous[END_COL]:
                 p = previous[:END_COL+1]
-                p.extend([previous[STRAND_COL],previous[samples_col]])
+                p.extend([previous[strand_col],previous[samples_col]])
                 sys.stdout.write("%s\n" % ("\t".join(p)))
                 previous = current
             #same junction add to the sample IDs/coverages
@@ -89,12 +92,12 @@ def merge(args):
     #last one
     if len(previous) > 0:
         p = previous[:END_COL+1]
-        p.extend([previous[STRAND_COL],previous[samples_col]])
+        p.extend([previous[strand_col],previous[samples_col]])
         sys.stdout.write("%s\n" % ("\t".join(p)))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='junction merge')
-    parser.add_argument('--list-file', metavar='/path/to/file_with_queries', type=str, default=None, help='path to a file with list of junction files to aggregate and optionally their sample IDs (rail_ids) and study names)')
+    parser.add_argument('--list-file', metavar='/path/to/manifest_file_of_per_sample_jx_files', type=str, default=None, help='path to a file with list of junction files to aggregate and optionally their sample IDs (rail_ids) and study names)')
     parser.add_argument('--gzip', action='store_const', const=True, default=False, help='input files are gzipped')
     parser.add_argument('--append-samples', action='store_const', const=True, default=False, help='set this if aggregating beyond sample-level results')
     args = parser.parse_args()
