336f002655f0a726d3e776a6d3bfa35863a76d66
ChristopherWilks <broadsword@gmail.com>
2018-12-06 18:49:52 -0500

    added load/store of prefix ID counters

--- sample_ids/assign_compilation_ids.py
+++ sample_ids/assign_compilation_ids.py
@@ -1,5 +1,6 @@
 #!/bin/env python2
 import sys
+import shutil
 import argparse
 from collections import Counter
 import buildIDs
@@ -12,8 +13,13 @@ def main(args):
     compilation_code = ""
     if args.compilation_code != -1:
         compilation_code = str(bin(args.compilation_code)[2:].zfill(COMPILATION_CODE_BIT_LENGTH))
-    fin = open(args.accessions_file, "rb")
     prefix_counters = Counter()
+    if args.group_counters_file is not None:
+        with open(args.group_counters_file, "r") as fin:
+            for line in fin:
+                (group, count) = line.rstrip().split('\t')
+                prefix_counters[str(group)] = int(count)
+    fin = open(args.accessions_file, "rb")
     for line in fin:
         if counter == -1 and not args.no_header:
             counter+=1
@@ -24,6 +30,7 @@ def main(args):
             sys.exit(-1)
         acc = fields[args.acc_col]
         prefix_str = buildIDs.encodeString(acc, buildIDs.alphaParseSuffix)
+        #sys.stderr.write(prefix_str+'\n')
         #use prefix-relative counters
         counter_str = str(bin(prefix_counters[prefix_str])[2:])
         prefix_counters[prefix_str] += 1
@@ -31,12 +38,16 @@ def main(args):
         #sys.stdout.write(str(int(prefix_ctr_str,2))+"\t"+acc+\t"+"\n")
         sys.stdout.write(fields[0]+"\t"+acc+"\t"+str(int(prefix_ctr_str,2))+"\n")
         counter+=1
+    shutil.move(args.group_counters_file, "%s.old" % (args.group_counters_file))
+    with open(args.group_counters_file, "w") as fout:
+        [fout.write("%s\t%s\n" % (x, str(prefix_counters[x]))) for x in prefix_counters.keys()]
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Assigns IDs for a specified compilation')
 
     parser.add_argument('--accessions-file', metavar='/path/to/file_with_accessions', type=str, default=None, help='path to file with list of accessions to assign IDs to')
+    parser.add_argument('--group-counters-file', metavar='/path/to/file_with_current_group_id_counters', type=str, default=None, help='path to file with the list of groups (low order digits) and their last used id counts')
     parser.add_argument('--acc-col', metavar='integer column #', type=int, default=0, help='column number in --accessions-file where accession is (defaults to 0)')
     parser.add_argument('--compilation-code', metavar='8-bit integer code per compilation', type=int, default=-1, help='used as a prefix to the sample ID to differentiate samples from different compilations when results are merged across compilations; 15 (1111) is reserved for local (non-public) compilations.  Default is not to include the compilation ID in the ID generation.')
     parser.add_argument('--no-header', action='store_const', const=True, default=False, help='if --accessions-file doesn\'t have a header')
