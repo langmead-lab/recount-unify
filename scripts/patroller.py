4896bb74ef073c8096bddf4c32be5054c26d8d69
ChristopherWilks <broadsword@gmail.com>
2018-12-10 15:51:08 -0500

    working file search in patroller

--- scripts/patroller.py
+++ scripts/patroller.py
@@ -7,6 +7,7 @@ import glob
 from pathlib import Path
 import re
 import subprocess
+import argparse
 
 #stubbing the basic set of compilations here before we code the
 #interface to the remote DB
@@ -18,8 +19,9 @@ SNAKEMAKE_PATH = 'snakemake'
 
 #in sec
 SLEEP=10
-PUMP_DIR="./sra"
-DEST_DIR="./pre_staging"
+PUMP_DIR="./srav1"
+DEST_DIR="./staging"
+OUTPUT_DIR="./unified"
 
 def run_command(cmd_args, cmd_name, datetime_stamp):
     for param in ['>', '%s.%s' % (cmd_name, datetime_stamp), '2>&1']:
@@ -28,7 +30,7 @@ def run_command(cmd_args, cmd_name, datetime_stamp):
     print ("running %s" % cmd_args.join(" "))
     try:
         cp = subprocess.run(cmd_args, shell=True, check=True, text=True) 
-    except CalledProcesserror cpe:
+    except subprocess.CalledProcesserror as cpe:
         sys.stdout.write("error in run_command for command: %s\n" % (cmd_args.join(" ")))
         raise cpe
 
@@ -53,11 +55,12 @@ def check_study_status(args, study):
         if not os.path.exists(sample_ids_file):
             #generate sample ids file for study
             pass
-    return (True, sample_ids_file)
+    #TODO: return false for now
+    return (False, sample_ids_file)
 
 def generate_snakemake_cmd_args(args, output_dir, sample_ids_file):
     cmd_args = [SNAKEMAKE_PATH,'-j%d' % args.num_sm_proc, args.snakefile, '-r', '--config']
-    cmd_args.append('input=%s' % args.pre_staging_dir)
+    cmd_args.append('input=%s' % args.staging_dir)
     cmd_args.append('staging=%s' % output_dir)
     cmd_args.append('sample_ids_file=%s' % sample_ids_file)
     cmd_args.append('annotated_sjs=%s' % args.annotated_sj_file)
@@ -71,63 +74,82 @@ def generate_snakemake_cmd_args(args, output_dir, sample_ids_file):
 
 
 def process_study(args, study, study_map, sample_ids_file):
-    dest_dir = args.pre_staging_dir
+    dest_dir = args.staging_dir
     if not os.path.exists(os.path.join(dest_dir, study)):
-        Path(os.path.join(dest_dir, study)).mkdir()
+        Path(os.path.join(dest_dir, study)).mkdir(parents=True, exist_ok=True)
     ds = datetime.datetime.now().timestamp()
     fout = open(os.path.join(dest_dir, study, "attempts.moved.%s" % (ds)), "w")
     for job in study_map.keys():
         (attempt_num, fpath, loworder) = study_map[job]
+        f = fpath.split(os.path.sep)[-1]
         fout.write("%s\n" % (fpath)) 
-        shutil.copytree(fpath, os.path.join(dest_dir, study, loworder, job+'_'+attempt_num ))
-        if args.remove_incoming:
-            shutil.rmtree(fpath)
+        #TODO: this is temporary (making soft links), need to hardlink, then delete
+        d = os.path.join(dest_dir, study, loworder, job+'_'+attempt_num)
+        if not os.path.exists(d):
+            Path(d).mkdir(parents=True, exist_ok=True)
+        os.symlink(fpath, os.path.join(d, f))
+        #shutil.copytree(fpath, os.path.join(dest_dir, study, loworder, job+'_'+attempt_num ))
+        #if args.remove_incoming:
+        #    shutil.rmtree(fpath)
     fout.close()
 
     output_dir = os.path.join(args.output_dir, study)
     if not os.path.exists(output_dir):
-        Path(output_dir).mkdir()
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
     #now fire up snakemake on DEST_DIR/study
     snakemake_cmd_args = generate_snakemake_cmd_args(args, output_dir, sample_ids_file)
-    run_command(snakemake_cmd_args, 'snakemake', ds)
+    print (snakemake_cmd_args)
+    #run_command(snakemake_cmd_args, 'snakemake', ds)
 
         
 def main():
     parser = argparse.ArgumentParser(description='recount-unify script to "patrol" the directories containing processed files from recount-pump and move them to a pre-staging directorty, and then run the jx merge/annotate and exon sum paste steps (recount-unify) on them.')
     parser.add_argument('--incoming-dir', metavar='/path/to/dir_containing_pump_processed_files', type=str, default=PUMP_DIR, help='the path where recount-pump dumps it\'s procssed files.')
-    parser.add_argument('--pre-staging-dir', metavar='/path/to/dir_to_store_finished_pump_processed_files', type=str, default=DEST_DIR, help='the path where recount-pump runs which have the *.done file present, are moved.')
-    parser.add_argument('--output-dir', metavar='/path/to/dir_to_store_recount-unify_output', type=str, default=DEST_DIR, help='the path where in process and fully merged sj and exon sum files are stored.')
+    parser.add_argument('--staging-dir', metavar='/path/to/dir_to_store_finished_pump_processed_files', type=str, default=DEST_DIR, help='the path where recount-pump runs which have the *.done file present, are moved.')
+    parser.add_argument('--output-dir', metavar='/path/to/dir_to_store_recount-unify_output', type=str, default=OUTPUT_DIR, help='the path where in process and fully merged sj and exon sum files are stored.')
     parser.add_argument('--remove-incoming', action='store_const', const=True, default=False, help='removes the source files from the recount-pump incoming directories after they\'ve been copied to the pre-staging area')
+    parser.add_argument('--snakefile', metavar='/path/to/file_with_snakemake_rules', type=str, required=True, help='this file contains the list of rules making up recount-unify (sj merging/annotating & exon sum pasting).')
+    parser.add_argument('--num-sm-proc', metavar='1', type=int, default=1, help='# of Snakemake parallel processes to run, defaults to 1')
+    #snakemake specific parameters
     parser.add_argument('--annotated-sj-file', metavar='/path/to/file_with_annotated_junctions', type=str, required=True, help='this file contains a list of junctions from one or more annotations.')
     parser.add_argument('--existing-sj-file', metavar='/path/to/file_with_existing_junctions', type=str, default=None, help='if merging these new splice junctions with an already merged set of splice junctions')
     parser.add_argument('--existing-exon-sums-file', metavar='/path/to/file_with_existing_exon_sums', type=str, default=None, help='if merging these new exon sums with an already pasted set of exon sample sums OR just the original BED file/coordinates.')
     parser.add_argument('--sample-ID-file', metavar='/path/to/file_with_sample_IDs_mapped_to_study_and_run_accessions', type=str, default=None, help='this file contains the pre-generated mapping between recount-unify assigned sample IDs and study/run accessions/UUIDs. If not provided and not already present in the default location (<pre-staging>/<study-accession>.samples.tsv) it will be generated.')
-    parser.add_argument('--snakefile', metavar='/path/to/file_with_snakemake_rules', type=str, required=True, help='this file contains the list of rules making up recount-unify (sj merging/annotating & exon sum pasting).')
-    parser.add_argument('--num-sm-proc', metavar='1', type=int, default=1, help='# of Snakemake parallel processes to run, defaults to 1')
+
+    args = parser.parse_args()
 
     seen = {}
     fpath_patt = re.compile(r'^((.+)_attempt(\d+)).done$')
     loop = True
     while(loop):
-        files = search_for_files(args.pump_dir)
+        files = search_for_files(args.incoming_dir)
         for f in files:
-            #proj1_input44_attempt7.done
+            #args.pump_dir/??/study/proj#_input#_attempt#.done
+            #example: srav1/39/SRP008339/proj1_input5472_attempt0.done
             m = fpath_patt.search(f)
             assert(m)
+            #e.g. srav1/39/SRP008339/proj1_input5472_attempt0
             fdir = m.group(1)
+            #e.g. srav1/39/SRP008339/proj1_input5472
             fkey = m.group(2)
+            #e.g. 1
             attempt_num = m.group(3)
 
             fields = f.split('/')
+            #e.g. proj1_input5472_attempt0.done 
             fname = fields[-1]
-            loworder = fields[-2]
-            study = fields[-3]
+            #e.g. SRP008339
+            study = fields[-2]
+            #e.g. 39
+            loworder = fields[-3]
 
             if study not in seen:
                 seen[study]={}
             if fkey not in seen[study] or seen[study][fkey][0] > attempt_num:
                 seen[study][fkey] = [attempt_num, fdir, loworder]
-        
+       
+        print (seen)
+ 
         for study in seen.keys():
             #find out:
             #1) study started processing?
