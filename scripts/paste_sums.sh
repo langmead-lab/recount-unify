f5b5e5a30d5700799e82f78f111c8621f1790d8e
ChristopherWilks <broadsword@gmail.com>
2018-12-11 18:03:30 -0500

    fixed paste_sums to always output gzip for the final paste

--- scripts/paste_sums.sh
+++ scripts/paste_sums.sh
@@ -8,7 +8,7 @@ existing_sums=$4
 
 if [ -n "$dont_get_ids" ]; then
 #if set, we're doing a final paste of all previously pasted (or copied) sample groups so no need to handle sample IDs
-    cat $manifest | perl -ne 'chomp; $f=$_; $s.=" $f"; $c++; END { if($c > 1) { print "paste $s\n"; `paste $s | gzip > '${output}'`; } else { `cp $f '${output}'`; }}'
+    cat $manifest | perl -ne 'chomp; $f=$_; $s.=" $f"; $c++; END { if($c > 1) { print "paste $s\n"; `paste $s | gzip > '${output}'`; } else { `cat $f | gzip > '${output}'`; }}'
 else
 #here we need to make sure we output the correct order of samples IDs as the column header
     cut -f 2,3 $manifest | perl -ne 'chomp; ($rid,$f)=split(/\t/,$_); $h.="$rid\t"; $s.=" $f.unc"; $c++; END { $h=~s/\t$//; open(OUT,">'${output}'"); print OUT "$h\n"; close(OUT); if($c > 1) { print "paste $s\n"; `paste $s >> '${output}'`; } else { `cat $f.unc >> '${output}'`; }}'
