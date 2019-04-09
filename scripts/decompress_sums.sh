3ef4c6251c8adef9fdb00440ee7c41ea16658ae7
ChristopherWilks <broadsword@gmail.com>
2019-04-09 18:01:42 -0400

    add sort to decompress script

--- scripts/decompress_sums.sh
+++ scripts/decompress_sums.sh
@@ -4,6 +4,6 @@ set -o pipefail -o nounset -o errexit
 manifest=$1
 output_sentinel=$2
 #run one decompress job at a time, but run it for all of the files listed in the manifest
-cut -f 3 $manifest | xargs -n 1 -P 1 -I{} sh -c 'zstd -cd $1 | cut -f 5 > ${1}.unc' -- {}
+cut -f 3 $manifest | xargs -n 1 -P 1 -I{} sh -c 'zstd -cd $1 | sort -k1,1 -k2,2n -k3,3n | cut -f 4 > ${1}.unc' -- {}
 #when done, write a "done" sentinel
 touch $output_sentinel
