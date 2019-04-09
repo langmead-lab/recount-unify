3e22e2387c8c2de3b05b472a4bc341b92c05825f
ChristopherWilks <broadsword@gmail.com>
2019-04-09 15:19:02 -0400

    updated finder scripts to match path changes

--- scripts/find_done.sh
+++ scripts/find_done.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 set -o pipefail -o nounset -o errexit 
 
-#date=`date +%Y%m%d.%s`
 date=`date +%s`
 
 search_dir=$1
 destination_dir=$2
-#samples=$3
 
 #make top level (study) pre-staging directory
-perl -e 'if(! -d "'${destination_dir}'") { `mkdir -p '${destination_dir}'`; }'
+mkdir -p ${destination_dir}
 
-#make loworder (per-study) pre-staging directories
-#UPDATE: dont do this, leads to errors downstream when empty directories are picked up
-#perl -e 'if(! -d "'${destination_dir}'/99") { for $i (0..99) { $i1=$i; $i1="0".$i1 if(length($i) == 1); `mkdir '${destination_dir}'/$i1`;} }'
-
-#now 1) rsync the latest attempt directory which has a .done file and 2) delete the rest of the attemps/remaining
-#find $search_dir -name "*.done" | cut -d'.' -f 1 | sort | perl -ne 'BEGIN { $dest_dir="'${destination_dir}'"; } chomp; $fp=$_; @f=split(/\//,$fp); $f=pop(@f); $loworder=pop(@f); ($proj,$input_id,$attempt)=split(/_/,$f); $f_wo_attempt_num=$f; $f_wo_attempt_num=~s/\d+$//; $h{$f_wo_attempt_num}=[$fp,$loworder]; END { for $v (values %h) { ($fp,$loworder)=@$v; if(! -d "$dest_dir/$loworder") { `mkdir -p $dest_dir/$loworder`; } print "$fp\n"; `rsync -av $fp $dest_dir/$loworder/`; $fp_no_attempt=$fp; $fp_no_attempt=~s/\d+$//; `rm -rf $fp_no_attempt*`; }}' > ${destination_dir}/attempts.moved.${date}
-#dont do the rm yet
-#also do hard links instead of rsyncs
-#find -L $search_dir -name "*.done" | sort | perl -ne 'BEGIN { open(IN,"<"'${samples}'"); while($line=<IN>) { chomp($line); ($rid,$srr,$srp)=split(/\t/,$line); $h{$srr}=$srp; } close(IN); $dest_dir="'${destination_dir}'"; } chomp; $fp=$_; $fp=~s/\.done$//; @f=split(/\//,$fp); $f=pop(@f); $study=pop(@f); $loworder=pop(@f); ($proj,$input_id,$attempt)=split(/_/,$f); $f_wo_attempt_num=$f; $f_wo_attempt_num=~s/(\d+)$//; $attempt_num=$1; print "$study\t$f\t$loworder\t$f_wo_attempt_num\t$attempt_num\n"; if(!$h{$study.$f_wo_attempt_num} || $h{$study.$f_wo_attempt_num}->[0] > $attempt_num) { $h{$study.$f_wo_attempt_num}=[$attempt_num,$fp,$loworder,$study]; } END { for $v (values %h) { ($attempt_num,$fp,$loworder,$study)=@$v; if(! -d "$dest_dir/$loworder/$study") { `mkdir -p $dest_dir/$loworder/$study`; } print "$fp\n"; `ln $fp $dest_dir/$loworder/$study/`; }}' > ${destination_dir}/attempts.moved.${date}
-find -L $search_dir -name "*.done" | sort | perl -ne 'BEGIN { $dest_dir="'${destination_dir}'"; } chomp; $fp=$_; $fp=~s/\.done$//; @f=split(/\//,$fp); $f=pop(@f); $study=pop(@f); $loworder=pop(@f); ($proj,$input_id,$attempt)=split(/_/,$f); $f_wo_attempt_num=$f; $f_wo_attempt_num=~s/(\d+)$//; $attempt_num=$1; print "$study\t$f\t$loworder\t$f_wo_attempt_num\t$attempt_num\n"; if(!$h{$study.$f_wo_attempt_num} || $h{$study.$f_wo_attempt_num}->[0] > $attempt_num) { $h{$study.$f_wo_attempt_num}=[$attempt_num,$fp,$loworder,$study]; } END { for $v (values %h) { ($attempt_num,$fp,$loworder,$study)=@$v; if(! -d "$dest_dir/$loworder/$study") { `mkdir -p $dest_dir/$loworder/$study`; } print "$fp\n"; `ln $fp $dest_dir/$loworder/$study/`; }}' > ${destination_dir}/attempts.moved.${date}
+find -L $search_dir -name "*.done" | sort | perl -ne 'BEGIN { $dest_dir="'${destination_dir}'"; } chomp; $fp=$_; $fp=~s/\.done$//; @f=split(/\//,$fp); $f=pop(@f); $acc=pop(@f); $acc_loworder=pop(@f); $study=pop(@f); $loworder=pop(@f); ($proj,$input_id,$attempt)=split(/_/,$f); $f_wo_attempt_num=$f; $f_wo_attempt_num=~s/(\d+)$//; $attempt_num=$1; print "$study\t$f\t$loworder\t$f_wo_attempt_num\t$attempt_num\t$acc_loworder\n"; if(!$h{$study.$f_wo_attempt_num} || $h{$study.$f_wo_attempt_num}->[0] > $attempt_num) { $h{$study.$f_wo_attempt_num}=[$attempt_num,$fp,$loworder,$study,$acc_loworder,$acc]; } END { for $v (values %h) { ($attempt_num,$fp,$loworder,$study,$acc_loworder,$acc)=@$v; `mkdir -p $dest_dir/$loworder/$study/$acc_loworder/$acc`; print "$fp\n"; `ln -s $fp $dest_dir/$loworder/$study/$acc_loworder/$acc/`; }}' > ${destination_dir}/attempts.moved.${date}
--- scripts/find_new_files.sh
+++ scripts/find_new_files.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -o pipefail -o nounset -o errexit 
 
+DELIM='!';
+
+#top level of incoming dir
+#(where fully done analyses were copied/symlinked from original set of attempts)
 search_dir=$1
 #map between Rail generated IDs and externall accessions/UUIDs, format: rail_id<TAB>accession
 sample_ids_file=$2
 destination_dir=$3
+#e.g. "unique.exon_bw_count" or "sj" or "all.exon_bw_count"
 analysis_type=$4
+#.e.g ".zst"
 additional_suffix=$5
 
-find -L $search_dir -name "*.${analysis_type}${additional_suffix}" -size +0 | perl -ne 'BEGIN { open(IN,"<'${sample_ids_file}'"); %rids; while($line=<IN>) { chomp($line); @f=split(/\t/,$line); $rid=$f[0]; $run_acc=$f[1]; $rids{$run_acc}=$rid; } close(IN); } chomp; $f=$_; `rsync -av $f '${destination_dir}'/`; @f=split(/\//,$f); $fname=pop(@f); $fname=~/^([^_]+)_/; $run_acc=$1; $attempt=pop(@f); $loworder=pop(@f); $rid=$rids{$run_acc}; push(@{$h{$loworder}},["'${destination_dir}'/$fname.filtered.sorted.gz",$rid,"'${destination_dir}'/$fname",$run_acc]); END { open(ALL_OUT,">'${destination_dir}'/'${analysis_type}'.groups.manifest"); for $k (keys %h) { open(OUT,">'${destination_dir}'/'${analysis_type}'.$k.manifest"); for $a (@{$h{$k}}) { print ALL_OUT "".$a->[3]."\t'${analysis_type}'.$k.manifest\n"; print OUT "".join("\t",@$a)."\n"; } close(OUT); } close(ALL_OUT); }'
+find -L $search_dir -name "*.${analysis_type}${additional_suffix}" -size +0 | perl -ne 'BEGIN { open(IN,"<'${sample_ids_file}'"); %rids; while($line=<IN>) { chomp($line); @f=split(/\t/,$line); $rid=$f[2]; $run_acc=$f[1]; $rids{$run_acc}=$rid; } close(IN); } chomp; $f=$_; `rsync -av $f '${destination_dir}'/`; @f=split(/\//,$f); $fname=pop(@f); $fname=~/^([^!]+)!/; $run_acc=$1; $attempt=pop(@f); $acc=pop(@f); $acc_loworder=pop(@f); $rid=$rids{$run_acc}; push(@{$h{$acc_loworder}},["'${destination_dir}'/$fname.filtered.sorted.gz",$rid,"'${destination_dir}'/$fname",$run_acc]); END { open(ALL_OUT,">'${destination_dir}'/'${analysis_type}'.groups.manifest"); for $k (keys %h) { open(OUT,">'${destination_dir}'/'${analysis_type}'.$k.manifest"); for $a (@{$h{$k}}) { print ALL_OUT "".$a->[3]."\t'${analysis_type}'.$k.manifest\n"; print OUT "".join("\t",@$a)."\n"; } close(OUT); } close(ALL_OUT); }'
