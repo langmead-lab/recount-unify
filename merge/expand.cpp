cbc9cf25b127837c74476353506ac119a9c3fa40
ChristopherWilks <broadsword@gmail.com>
2019-04-17 02:06:46 -0400

    further progress on expand

--- merge/expand.cpp
+++ merge/expand.cpp
@@ -27,6 +27,7 @@ static const int END_COL=8;
 
 static const int GENE_COL=9;
 static const int STRAND_COL=11;
+static const int COUNTS_START_COL=6;
 
 //first 6 columns are the key field for matching between
 //the annotation map and the disjoint exon sums
@@ -41,25 +42,24 @@ static const int MAX_REGION_BUFFER_SZ=500;
 //TODO2: figure this out automatically
 static const int NUM_SAMPLES=1393;
 
+typedef std::vector<std::string> strlist;
 typedef std::vector<char*> charlist;
 typedef hash_map<std::string, int> charmap;
 
 typedef struct {
-    /*char* name;
-    long start;
-    long end;*/
     //gene names that have this exon
     charmap* names;
     //raw read count per sample
-    unsigned long* counts;
+    uint32_t* counts;
 } annotation_t;
 
 //track disjoint exon "keys" (chrm,start,end,name,score,strand) to array indexes
 //for actual genes/exons from annotation
 //list of actual annotations (annotation_t structs) for gene/exons
 typedef hash_map<std::string, annotation_t*> annotation_t_map_t;
-typedef hash_map<std::string, charlist*> annotation_map_t;
-static const int process_region_line(char* line, const char* delim, annotation_map_t* amap, char*** key_fields, annotation_t_map_t* alist) {
+//typedef hash_map<std::string, strlist*> annotation_map_t;
+typedef hash_map<std::string, charmap*> annotation_map_t;
+static const int process_region_line(char* line, const char* delim, annotation_map_t* amap, char*** key_fields, annotation_t_map_t* alist, uint32_t** counts, int last_col) {
 	char* line_copy = strdup(line);
 	char* tok = strtok(line_copy, delim);
 	int i = 0;
@@ -74,7 +74,6 @@ static const int process_region_line(char* line, const char* delim, annotation_m
     char* key = new char[1024];
 
     int ret = 0;
-	int last_col = STRAND_COL;
 	while(tok != nullptr) {
 		if(i > last_col)
 			break;
@@ -85,57 +84,66 @@ static const int process_region_line(char* line, const char* delim, annotation_m
             continue;
         }
         //make the first (disjoint exon) key
-        else if(i == KEY_FIELD_COL_END+1)
+        else if(i == KEY_FIELD_COL_END+1) {
             sprintf(key,"%s\t%s\t%s\t%s\t%s\t%s",(*key_fields)[0],(*key_fields)[1],(*key_fields)[2],(*key_fields)[3],(*key_fields)[4],(*key_fields)[5]);
+            //fprintf(stderr,"key: %s\n",key);
+        }
         if(i == CHRM_COL) {
-			chrm = strdup(tok);
+            chrm = strdup(tok);
             memcpy((*key_fields)[i-CHRM_COL],tok,strlen(tok)+1);
         }
         else if(i == START_COL) {
-			start = atol(tok);
+            start = atol(tok);
             memcpy((*key_fields)[i-CHRM_COL],tok,strlen(tok)+1);
         }
         else if(i == END_COL) {
-			end = atol(tok);
+            end = atol(tok);
             memcpy((*key_fields)[i-CHRM_COL],tok,strlen(tok)+1);
         }
         else if(i == GENE_COL)
-			gname = strdup(tok);
+            gname = strdup(tok);
         else if(i == STRAND_COL) {
-			strand = strdup(tok);
+            strand = strdup(tok);
             memcpy((*key_fields)[i-CHRM_COL],tok,strlen(tok)+1);
         }
+
 		i++;
 		tok = strtok(nullptr, delim);
 	}
     //make 2nd key (original annotated exon)
-    char* key2 = new char[1024];
-    sprintf(key2,"%s\t%s\t%s\t%s",(*key_fields)[0],(*key_fields)[1],(*key_fields)[2],(*key_fields)[5]);
+    char* key2_ = new char[1024];
+    sprintf(key2_,"%s\t%s\t%s\t%s",(*key_fields)[0],(*key_fields)[1],(*key_fields)[2],(*key_fields)[5]);
+    std::string key2(key2_);
+    //fprintf(stderr,"key2: %s\n",key2);
    
     //establish map between disjoint exon key and
     //original annotated exon key 
     bool free_key = true;
     if(amap->find(key) == amap->end()) {
-        (*amap)[key] = new charlist[1];
+        //(*amap)[key] = new strlist[1];
+        (*amap)[key] = new charmap[1];
         free_key = false;
     }
-    (*amap)[key]->push_back(key2);
-    if(free_key)
-        delete key;
 
     //now store the original annotated exon
-    free_key = true;
-    if(alist->find(key2) == alist->end()) {
+    bool free_key2 = true;
+    auto it = alist->find(key2);
+    if(it == alist->end()) {
         annotation_t* coords = new annotation_t[1];
         coords->names = new charmap[1];
-        coords->counts = nullptr;
+        coords->counts = *counts;
         (*alist)[key2] = coords;
-        free_key = false;
+        free_key2 = false;
     }
+    else {
+        delete key2_;
+        key2 = it->first;
+    }
+    (*(*amap)[key])[key2]=1;
+    if(free_key)
+        delete key;
     //add gene name to exon's gene map
     (*(*alist)[key2]->names)[gname]=1;
-    if(free_key)
-        delete key2;
 
     if(line_copy)
 		free(line_copy);
@@ -143,8 +151,51 @@ static const int process_region_line(char* line, const char* delim, annotation_m
 		free(line);
     return ret;
 }
+
+typedef std::vector<uint32_t*> intlist;
+static const int process_counts_line(char* line, const char* delim, annotation_map_t* amap, char*** key_fields, annotation_t_map_t* alist, intlist* counts_list) {
+	char* line_copy = strdup(line);
+	char* tok = strtok(line_copy, delim);
+    char* key = new char[1024];
+	int i = 0;
+    int ret = 0;
+    //strlist* real_exons = nullptr;
+    charmap* real_exons = nullptr;
+	while(tok != nullptr) {
+        if(i <= KEY_FIELD_COL_END) {
+            memcpy((*key_fields)[i],tok,strlen(tok)+1);
+            i++;
+    		tok = strtok(nullptr, delim);
+            continue;
+        }
+        //make the first (disjoint exon) key
+        else if(i == KEY_FIELD_COL_END+1) {
+            sprintf(key,"%s\t%s\t%s\t%s\t%s\t%s",(*key_fields)[0],(*key_fields)[1],(*key_fields)[2],(*key_fields)[3],(*key_fields)[4],(*key_fields)[5]);
+            //fprintf(stderr,"key:%s\n",key);
+            real_exons = (*amap)[key];
+            for(auto const& kv: *real_exons) {
+                //fprintf(stderr,"%s\ti=%d %s %s\n",line,i,key,kv.first.c_str());
+                uint32_t* c = (*alist)[kv.first]->counts;
+                counts_list->push_back(c);
+            }
+        }
+        int k = i - COUNTS_START_COL;
+        //TODO: from here for each count, loop through the set of count arrays, ++ each array at position k
+        //for(auto const& e: *counts_list) {
+		i++;
+		tok = strtok(nullptr, delim);
+    }
+   
+    counts_list->clear(); 
+    delete key;
+    if(line_copy)
+		free(line_copy);
+	if(line)
+		free(line);
+    return ret;
+}
     
-static const int read_annotation(FILE* fin, annotation_map_t* amap) {
+static const int read_annotation(FILE* fin, annotation_map_t* amap, annotation_t_map_t* alist, uint32_t*** counts) {
     //temporarily holds the distinct fields used for the matching key
     char** key_fields = new char*[KEY_FIELD_COL_END+1];
     for(int i=0;i<=KEY_FIELD_COL_END;i++)
@@ -154,25 +205,55 @@ static const int read_annotation(FILE* fin, annotation_map_t* amap) {
 	assert(fin);
 	ssize_t bytes_read = getline(&line, &length, fin);
 	int err = 0;
-    annotation_t_map_t alist;
+    int counts_idx = 0;
 	while(bytes_read != -1) {
-	    err = process_region_line(strdup(line), "\t", amap, &key_fields, &alist);
+	    err = process_region_line(strdup(line), "\t", amap, &key_fields, alist, &((*counts)[counts_idx++]), STRAND_COL);
         assert(err==0);
 		bytes_read = getline(&line, &length, fin);
     }
     delete[] key_fields;
-	fprintf(stderr,"building annotation set done, disjoint2annot map size: %lu, original annotation map size: %lu\n", amap->size(), alist.size());
-    /*for(auto const& kv : *amap)
-        fprintf(stderr,"%s %lu\n", kv.first.c_str(), (kv.second)->size());*/
+	fprintf(stderr,"building annotation set done, disjoint2annot map size: %lu, original annotation map size: %lu\n", amap->size(), alist->size());
+    /*for(auto const& kv : *amap) {
+        fprintf(stderr,"%s %lu\n", kv.first.c_str(), (kv.second)->size());
+        for(auto const& kv2 : *kv.second)
+            fprintf(stderr,"\t%s\n",kv2.first.c_str());
+    }*/
     return err;
 }
 
 void go(std::string annotation_map_file, std::string disjoint_exon_sum_file)
 {
+    //start with static count matrix
+    uint32_t** counts = new uint32_t*[1024];
+    for(int i = 0; i < NUM_SAMPLES; i++)
+        counts[i] = new uint32_t[NUM_SAMPLES];
+
     FILE* afp = fopen(annotation_map_file.c_str(), "r");
-    annotation_map_t annotations; 
-    int err = read_annotation(afp, &annotations);
+    annotation_map_t disjoint2annotation; 
+    annotation_t_map_t exon2counts;
+    int err = read_annotation(afp, &disjoint2annotation, &exon2counts, &counts);
     fclose(afp);
+    
+    FILE* fin = fopen(disjoint_exon_sum_file.c_str(), "r");
+    //temporarily holds the distinct fields used for the matching key
+    char** key_fields = new char*[KEY_FIELD_COL_END+1];
+    for(int i=0;i<=KEY_FIELD_COL_END;i++)
+        key_fields[i]=new char[100];
+
+	char* line = nullptr;
+	size_t length = 0;
+	ssize_t bytes_read = getline(&line, &length, fin);
+    err = 0;
+    int counts_idx = 0;
+    int last_col = NUM_SAMPLES+GENE_COL-1;
+    uint32_t* counts_temp = new uint32_t[NUM_SAMPLES];
+    intlist* counts_list = new intlist[1];
+	while(bytes_read != -1) {
+	    err = process_counts_line(strdup(line), "\t", &disjoint2annotation, &key_fields, &exon2counts, counts_list);
+        assert(err==0);
+		bytes_read = getline(&line, &length, fin);
+    }
+    delete[] key_fields;
 }
 
 int main(int argc, char* argv[])
