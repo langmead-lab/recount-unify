8a30c44972edd323d48b53efd3a1d57664be5dae
ChristopherWilks <broadsword@gmail.com>
2019-06-19 03:38:44 -0400

    adding check for empty decompressed sum files

--- scripts/decompress_sums.sh
+++ scripts/decompress_sums.sh
@@ -5,5 +5,6 @@ manifest=$1
 output_sentinel=$2
 #run one decompress job at a time, but run it for all of the files listed in the manifest
 cut -f 3 $manifest | xargs -n 1 -P 1 -I{} sh -c 'zstd -cd $1 | cut -f 4 > ${1}.unc' -- {}
+cut -f 3 $manifest | perl -ne 'chomp; $f=$_.".unc"; @s=stat($f); if($s[7]==0) { `cp blank_exon_sums $f`;}'
 #when done, write a "done" sentinel
 touch $output_sentinel
