e4733abd813258a213ff5bc36b4a86435b38fbc1
Christopher Wilks <cwilks3@jhu.edu>
2020-01-03 14:27:55 -0500

    corrected comments in sj filter script

--- scripts/filter_new_sjs.sh
+++ scripts/filter_new_sjs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -o pipefail -o nounset -o errexit 
 
-#filter out non-canonical splice junctions and sort by coordinates, while cutting out extraneous fields, and ? stranded junctions
+#reformat splice junctions and sort by coordinates, while cutting out extraneous fields, and ? stranded junctions
 #assumes $f's directory is writable
 
-#canoncals are the same as they were in Rail, GT-AG, GC-AG, and AT-AC
+#motifs are filled in later rather than here
 for f in `cut -f 3 ${1}`; do
     zstd -cd $f 2>/dev/null | perl -ne 'chomp; $f=$_; ($chr,$start,$end,$strand,$motif,$annotated,$num_uniques,$num_multis,$max_overhang)=split(/\t/,$f); $total_cov=$num_uniques+$num_multis; $strand=~tr/012/\?\+\-/; print "$chr\t$start\t$end\t$num_multis\t$total_cov\t$strand\tCC-CC\n";' | sort -k1,1 -k2,2n -k3,3n -u | gzip > ${f}.filtered.sorted.gz;
 done
