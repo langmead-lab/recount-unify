e6b552a1000b90e9ce0d20e0c86eecc948810526
Christopher Wilks <cwilks3@jhu.edu>
2020-02-03 15:08:59 -0500

    fixed start coordinate offset in final gene rejoin, take annotation sources as parameter

--- rejoin/rejoin_genes.py
+++ rejoin/rejoin_genes.py
@@ -9,6 +9,7 @@ mapf = sys.argv[1]
 #gene or exon
 annot_type = sys.argv[2]
 counts_headerF = sys.argv[3]
+annot_source = sys.argv[4]
 
 counts_header = ""
 with open(counts_headerF,"r") as fin:
@@ -21,9 +22,11 @@ with open(mapf,"r") as fin:
     for line in fin:
         line = line.rstrip()
         (chrm, gstart, gend, gname, typenum, strand)  = line.split('\t')
+        gstart = str(int(gstart)+1)
         gsplit = gname.split('.')
         gene_id = '.'.join(gsplit[:len(gsplit)-1])
         annot_src = gsplit[-1]
+        #annot_src = annot_source
         if annot_src not in annot_fhs:
             annot_fhs[annot_src] = open(annot_src+"."+annot_type+".sums.tsv","w")
             annot_fhs[annot_src].write("gene_id\ttotal_length\tchromosome\tstart\tend\t%s\n" % (counts_header))
@@ -37,6 +40,8 @@ for line in sys.stdin:
     line = line.rstrip()
     fields = line.split('\t')
     (chrm, gstart, gend, strand)  = fields[:4]
+    #account for 0-based start coordinate
+    gstart = str(int(gstart)+1)
     counts = "\t".join(fields[4:])
     #output is: Gene   bp_length       chromosome      start   end count1 count2 count3....
     outstr = "\t".join([str((int(gend) - int(gstart)) + 1), chrm, gstart, gend])+"\t"+counts+"\n"
