89e5920a9ad595de238aad53f454e55cb9b8420d
ChristopherWilks <broadsword@gmail.com>
2020-02-11 15:43:18 -0500

    fixed handling of annotation src in gene_id in rejoin genes script

--- rejoin/rejoin_genes.py
+++ rejoin/rejoin_genes.py
@@ -10,6 +10,7 @@ mapf = sys.argv[1]
 annot_type = sys.argv[2]
 counts_headerF = sys.argv[3]
 annot_source = sys.argv[4]
+annot_sources = set(annot_source.split(','))
 
 counts_header = ""
 with open(counts_headerF,"r") as fin:
@@ -24,9 +25,17 @@ with open(mapf,"r") as fin:
         (chrm, gstart, gend, gname, typenum, strand)  = line.split('\t')
         gstart = str(int(gstart)+1)
         gsplit = gname.split('.')
-        gene_id = '.'.join(gsplit[:len(gsplit)-1])
-        annot_src = gsplit[-1]
-        #annot_src = annot_source
+        annot_src = None
+        num_splits = len(gsplit)
+        found_idx = -1
+        for i,potential_src in enumerate(gsplit):
+            if potential_src in annot_sources:
+                annot_src = potential_src
+                del gsplit[i] 
+                break
+        if annot_src is None:
+            continue
+        gene_id = '.'.join(gsplit)
         if annot_src not in annot_fhs:
             annot_fhs[annot_src] = open(annot_src+"."+annot_type+".sums.tsv","w")
             annot_fhs[annot_src].write("gene_id\ttotal_length\tchromosome\tstart\tend\t%s\n" % (counts_header))
@@ -39,10 +48,10 @@ with open(mapf,"r") as fin:
 for line in sys.stdin:
     line = line.rstrip()
     fields = line.split('\t')
-    (chrm, gstart, gend, strand)  = fields[:4]
+    (gene_name, chrm, gstart, gend, glength, strand)  = fields[:6]
     #account for 0-based start coordinate
     gstart = str(int(gstart)+1)
-    counts = "\t".join(fields[4:])
+    counts = "\t".join(fields[6:])
     #output is: Gene   bp_length       chromosome      start   end count1 count2 count3....
     outstr = "\t".join([str((int(gend) - int(gstart)) + 1), chrm, gstart, gend])+"\t"+counts+"\n"
     key = "\t".join([chrm, gstart, gend, strand])
