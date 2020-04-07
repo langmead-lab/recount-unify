1f9f05bf9c265dc246211a73d330ba1a985a8f87
ChristopherWilks <broadsword@gmail.com>
2020-04-07 02:42:55 -0400

    updated annotation extractor with extensive set of mouse annotations, all gencodes

--- annotate/annotation/rip_annotated_junctions.py
+++ annotate/annotation/rip_annotated_junctions.py
@@ -50,21 +50,26 @@ import gzip
 import sys
 import re
 
+#latest gencode rel num
+CURRENT_MOUSE_REL_NUM=24
+
 hg_file2source = {"hg19/gencode.v19.annotation.gtf.gz":"gC19","hg19/refGene.txt.gz":"rG19","hg19/acembly.txt.gz":"aC19","hg19/ccdsGene.txt.gz":"cG19","hg19/vegaGene.txt.gz":"vG19","hg19/knownGene.txt.gz":"kG19","hg19/mgcGenes.txt.gz":"mG19","hg19/lincRNAsTranscripts.txt.gz":"lR19","hg19/sibGene.txt.gz":"sG19","hg38/refGene.txt.gz":"rG38","hg38/ccdsGene.txt.gz":"cG38","hg38/knownGene.txt.gz":"kG38","hg38/mgcGenes.txt.gz":"mG38","hg38/lincRNAsTranscripts.txt.gz":"lR38","hg38/sibGene.txt.gz":"sG38","hg38/chess2.2_assembly.gtf.gz":"cH38","hg38/gencode.v24.annotation.gtf.gz":"gC24","hg38/gencode.v25.annotation.gtf.gz":"gC25","hg38/gencode.v26.annotation.gtf.gz":"gC26","hg38/gencode.v33.annotation.gtf.gz":"gC33","hg38/gencode.v29.annotation.gtf.gz":"gC29"} #,"hg38/knownAlt.txt.gz":"kA38","hg19/knownAlt.txt.gz":"kA19"}
 
-m38_file2source = {"m38/knownGene.txt.gz":"kG38","m38/refGene.txt.gz":"rG38","m38/gencode.vM23.basic.annotation.gtf.gz":"gC23","m38/gencode.vM9.basic.annotation.gtf.gz":"gC09"}
+mg_file2source = {"m37/ccdsGene.txt.gz":"cG37","m37/refGene.txt.gz":"rG37","m37/mgcGenes.txt.gz":"mG37","m37/knownGene.txt.gz":"kG37","m37/vegaGene.txt.gz":"vG37","m37/acembly.txt.gz":"aC37","m38/GSE72311_lncrna.gtf.gz":"lR38","m38/mgcGenes.txt.gz":"mG38","m38/ccdsGene.txt.gz":"cG38","m38/knownGene.txt.gz":"kG38","m38/refGene.txt.gz":"rG38","m37/gencode.vM1.annotation.gtf.gz":"gC01","m37/fantom4_all_mrna.gtf.gz":"fM38","m37/fantom4_all_est.gtf.gz":"fE38"}
+for i in range(2,CURRENT_MOUSE_REL_NUM+1):
+    mg_file2source["m38/gencode.vM%d.annotation.gtf.gz" % (i)] = "gC%02.0f" % (i)
 
-file2sources = {'hg38':hg_file2source, 'hg19':hg_file2source, 'm38':m38_file2source}
+file2sources = {'hg38':hg_file2source, 'hg19':hg_file2source, 'm38':mg_file2source}
 
 def liftover(args, annotated_junctions_from, annotated_junctions_to):
     #can handle either direction for human, but not mouse
-    if args.org == 'hg38' or args.org == 'hg19':
+    #if args.org == 'hg38' or args.org == 'hg19':
+    if True:
         temp_from = os.path.join(extract_destination, 'from.bed')
         temp_to = os.path.join(extract_destination, 'to.bed')
         with open(temp_from, 'w') as from_stream:
             for i, junction in enumerate(annotated_junctions_from):
                 # Handle incorrect junctions
-                #print >>hg19_stream, '{}\t{}\t{}\tdummy_{}\t1\t{}'.format(
                 print >>from_stream, '{}\t{}\t{}\t{}\t1\t{}'.format(
                         junction[0], junction[1], junction[2], junction[4], junction[3]
                     )
@@ -86,26 +91,18 @@ def liftover(args, annotated_junctions_from, annotated_junctions_to):
                              'assembly, it\'s ignored.')
         with open(temp_to) as to_stream:
             for line in to_stream:
-                chrom, start, end, name, score, strand = line.strip().split(
-                                                                        '\t'
-                                                                    )[:6]
+                chrom, start, end, name, score, strand = line.strip().split('\t')[:6]
                 if chrom in refs:
-                    annotated_junctions_to.add(
-                        (chrom, int(start), int(end), strand, name)
-                    )
+                    annotated_junctions_to.add((chrom, int(start), int(end), strand, name))
                 else:
-                    print >>sys.stderr, '({}, {}, {}, {}) not recorded.'.format(
-                                                chrom, start, end, name
-                                            )
+                    print >>sys.stderr, '({}, {}, {}, {}) not recorded.'.format(chrom, start, end, name)
         after_liftover = len([junction for junction
                                 in annotated_junctions_to
                                 if junction[0] in refs])
         print >>sys.stderr, ('{} annotations contributed {} junctions, and '
                              'liftover of hg19 annotations contributed an '
                              'additional {} junctions.').format(args.org,
-                                    before_liftover,
-                                    after_liftover - before_liftover
-                                )
+                                    before_liftover,after_liftover - before_liftover)
 
 
 if __name__ == '__main__':
@@ -153,15 +150,12 @@ if __name__ == '__main__':
                                                 'extract_splice_sites.py')
     containing_dir = os.path.dirname(os.path.realpath(__file__))
     with open(args.org_sizes) as hg38_stream:
-        refs = set(
-                [tokens.strip().split('\t')[0] for tokens in hg38_stream]
-            )
+        refs = set([tokens.strip().split('\t')[0] for tokens in hg38_stream])
     annotated_junctions_hg19 = set()
     annotated_junctions_hg38 = set()
-    ref_patt = re.compile(r'\/(((hg38)|(m38)|(hg19))\/)')
+    ref_patt = re.compile(r'\/(((hg38)|(m38)|(hg19)|(m37))\/)')
     for junction_file in glob.glob(
-            os.path.join(extract_destination, 'anno', '*', '*')
-            ):
+            os.path.join(extract_destination, 'anno', '*', '*')):
         sys.stderr.write("about to extract jx from %s\n" % (junction_file))
         m = ref_patt.search(junction_file)
         if m is None:
@@ -174,27 +168,20 @@ if __name__ == '__main__':
             annotated_junctions = annotated_junctions_hg38
         else:
             annotated_junctions = annotated_junctions_hg19
-        if 'gencode' in junction_file or 'chess' in junction_file:
+        if 'gencode' in junction_file or 'chess' in junction_file or 'GSE72311' in junction_file or 'fantom' in junction_file:
             #extract_splice_sites_path prints 0-based, exon coords around junctions
             #hence the +2 for the start here
-            extract_process = subprocess.Popen(' '.join([
-                                            sys.executable,
+            extract_process = subprocess.Popen(' '.join([sys.executable,
                                             extract_splice_sites_path,
                                             '<(gzip -cd %s) > %s.extracted'
-                                               % (junction_file,junction_file)
-                                        ]),
+                                               % (junction_file,junction_file)]),
                                         shell=True,
                                         executable='/bin/bash',
-                                        stdout=subprocess.PIPE
-                                    )
+                                        stdout=subprocess.PIPE)
             exit_code = extract_process.wait()
             if exit_code != 0:
                 raise RuntimeError(
-                    'extract_splice_sites.py had nonzero exit code {}.'.format(
-                                                                    exit_code
-                                                                )
-                )
-            #for line in extract_process.stdout:
+                    'extract_splice_sites.py had nonzero exit code {}.'.format(exit_code))
             fin = open("%s.extracted" % (junction_file), "r")
             for line in fin:
                 tokens = line.strip().split('\t')
@@ -203,16 +190,12 @@ if __name__ == '__main__':
                 if tokens[2] < tokens[1]:
                     print >>sys.stderr, (
                             'Invalid junction ({}, {}, {}) from file {}. '
-                            'Skipping.'
-                        ).format(
-                                tokens[0], tokens[1], tokens[2], junction_file
-                            )
+                            'Skipping.').format(tokens[0], tokens[1], tokens[2], junction_file)
                     continue
                 tokens[-1] = datasource_code + ':::' + tokens[-1]
                 junction_to_add = tuple(tokens)
                 annotated_junctions.add(junction_to_add)
                 unique_junctions.add(junction_to_add)
-            #extract_process.stdout.close()
             fin.close()
         else:
             transcript_id_col = 1
@@ -226,21 +209,17 @@ if __name__ == '__main__':
                 exons = [(int(start), int(end)) for start, end in
                                 zip(tokens[8+offset].split(','),
                                     tokens[9+offset].split(','))[:-1]]
-                junctions_to_add = [
-                        (tokens[1+offset], exons[i-1][1] + 1, exons[i][0],
+                junctions_to_add = [(tokens[1+offset], exons[i-1][1] + 1, exons[i][0],
                          tokens[2+offset], "%s:::%s" % (datasource_code, tokens[transcript_id_col]))
-                        for i in xrange(1, len(exons))
-                    ]
+                        for i in xrange(1, len(exons))]
                 final_junctions_to_add = []
                 for junction in junctions_to_add:
                     if junction[2] < junction[1]:
                         print >>sys.stderr, (
                                 'Invalid junction ({}, {}, {}) from line {} from file {}. '
-                                'Skipping.'
-                            ).format(
+                                'Skipping.').format(
                                     junction[0], junction[1], junction[2], line,
-                                    junction_file
-                                )
+                                    junction_file)
                         continue
                     final_junctions_to_add.append(junction)
                 annotated_junctions.update(final_junctions_to_add)
@@ -251,7 +230,7 @@ if __name__ == '__main__':
             )
     #now do liftover---either direction depending on what's passed in via args.org
     annotated_junctions = annotated_junctions_hg38
-    if args.org == 'hg38':
+    if args.org == 'hg38' or args.org == 'm38':
         liftover(args, annotated_junctions_hg19, annotated_junctions_hg38)
     elif args.org == 'hg19':
         liftover(args, annotated_junctions_hg38, annotated_junctions_hg19)
