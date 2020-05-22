aeb5d6ad585bdb3d0d59764851fe9b21bdc5e132
Christopher Wilks <cwilks3@jhu.edu>
2020-05-22 17:35:45 -0400

    add delim, recount_exon_id to annotation map back to GTF script

--- scripts/map_monorail_coords_back_to_original_GTF.py
+++ scripts/map_monorail_coords_back_to_original_GTF.py
@@ -18,11 +18,11 @@ with open(sys.argv[1],"r") as fin:
         fields = line.rstrip().split(delim)
         if feat_type == 'gene':
             (gid, bp_length, chrm, start, end) = fields
-            coord_map['_'.join([gid,chrm,start,end])] = [idx,bp_length]
+            coord_map[delim.join([gid,chrm,start,end])] = [idx,bp_length]
             continue
-        (chrm, start, end) = fields
+        (chrm, start, end, strand) = fields
         bp_length = str((int(end) - int(start)) + 1)
-        coord_map['_'.join([chrm,start,end])] = [idx,bp_length]
+        coord_map[delim.join([chrm,start,end,strand])] = [idx,bp_length]
 
 
 #GL000008.2^ICurated Genomic^Iexon^I124376^I125329^I.^I-^I.^Itranscript_id "gene14440.R109"; gene_id "gene14440.R109"; gene_name "SNX18P15"; Dbxref "GeneID:100419019,HGNC:HGNC:39623"; Name "SNX18P15"; description "sorting nexin 18 pseudogene 15"; gbkey "Gene"; gene_biotype "pseudogene"; pseudo "true";
@@ -34,22 +34,23 @@ for line in sys.stdin:
     fields = line.rstrip().split('\t')
     if fields[2] != feat_type:
         continue
-    (chrm, start, end) = (fields[0], fields[3], fields[4])
+    (chrm, start, end, strand) = (fields[0], fields[3], fields[4], fields[6])
     fields[8] = fields[8].replace('.%s' % (annot),'')
-    key = '_'.join([chrm,start,end])
+    key = delim.join([chrm,start,end,strand])
     if feat_type == 'gene':
         m = gpatt.search(fields[8])
         if m is None:
             sys.stderr.write("MISSING_%s_id, skipping %s" % (feat_type, line))
             continue
         gid = m.group(1)
-        key = '_'.join([gid,chrm,start,end])
+        key = delim.join([gid,chrm,start,end])
         #some refseq genes have their chromosome as a suffix
         if key not in coord_map:
-            key = '_'.join([gid+'.%s'%(chrm),chrm,start,end])
+            key = delim.join([gid+'.%s'%(chrm),chrm,start,end])
     if key not in coord_map:
         sys.stderr.write("NOT_USED_IN_MONORAIL_%s, skipping %s" % (feat_type, line))
         continue
     fields[5] = coord_map[key][1]
+    recount_exon_id = ' recount_exon_id "%s";' % (key)
     #output a key we can sort later on to match the exact order of the sums file
-    sys.stdout.write(str(coord_map[key][0])+'\t'+'\t'.join(fields)+'\n') 
+    sys.stdout.write(str(coord_map[key][0])+'\t'+'\t'.join(fields)+recount_exon_id+'\n') 
