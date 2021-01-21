cbe6c1390bdb361003cf2c806872afc18b233fed
ChristopherWilks <broadsword@gmail.com>
2021-01-21 17:18:33 -0500

    further fixes to joining jx sample metadata

--- scripts/join_railID_to_sample_metadata.sh
+++ scripts/join_railID_to_sample_metadata.sh
@@ -11,5 +11,6 @@ sample_id_file=$1
 #format: <study_id>TAB<sample_id>....
 sample_original_metadata_file=$2
 
+#first print header:
 #we need to join the user's original sample metadata with the newly generated rail_ids, do that using the study and sample IDs
-cat $sample_original_metadata_file | perl -ne 'BEGIN { open(IN,"<'$sample_id_file'"); @ids=<IN>; close(IN); chomp(@ids); %idmap=(); map { ($study,$sample,$rid)=split(/\t/,$_); $idmap{$study."|".$sample}=$rid; } @ids; } chomp; $i++; $f=$_; @f=split(/\t/,$f); $study=shift(@f); $sample=shift(@f); $f=join("\t",@f); $f="\t$f" if(scalar @f > 0); $rail_id=$idmap{"$study|$sample"}; if(!$rail_id) { if($i==1) { print "rail_id\t$sample\t$study"."$f\n"; next; } print STDERR "failed to map $study\t$sample\t$f to a rail_id, terminating\n"; exit(-1); } print "$rail_id\t$sample\t$study"."$f\n";'
+cat $sample_original_metadata_file | perl -ne 'BEGIN { open(HDR,">'$sample_id_file'.new_header"); open(IN,"<'$sample_id_file'"); @ids=<IN>; close(IN); chomp(@ids); %idmap=(); map { ($study,$sample,$rid)=split(/\t/,$_); $idmap{$study."|".$sample}=$rid; } @ids; } chomp; $i++; $f=$_; @f=split(/\t/,$f); $study=shift(@f); $sample=shift(@f); $f=join("\t",@f); $f="\t$f" if(scalar @f > 0); $rail_id=$idmap{"$study|$sample"}; if(!$rail_id) { if($i==1) { print HDR "rail_id\t$sample\t$study"."$f\n"; next; } print STDERR "failed to map $study\t$sample\t$f to a rail_id, terminating\n"; close(HDR); exit(-1); } print "$rail_id\t$sample\t$study"."$f\n"; END { close(HDR); }'
