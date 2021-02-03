ebc4b1cc26c0a4d86df984d6b9c0be9aa45f76de
btrx-cwilks <cwilks@rbneuro.com>
2021-02-03 21:24:27 +0000

    clean up fields and lowercase fieldnames for loading into R

--- metadata/joiner.py
+++ metadata/joiner.py
@@ -25,9 +25,12 @@ parser.add_argument('--output-sep', metavar='separator', type=str, default='\t',
 parser.add_argument('--smaller-matching', action='store_const', const=True, default=False, help='don\'t print out unmatched rows from the smaller file')
 parser.add_argument('--larger-matching', action='store_const', const=True, default=False, help='don\'t print out unmatched rows from the larger file')
 
-parser.add_argument('--print-duplicate-join-fields', action='store_const', const=True, default=False, help='don\'t suppress the printing of additional (duplicate) join fields from all the files, default is to only print one of them (from --smaller file)')
+#parser.add_argument('--keep-case', action='store_const', const=True, default=False, help='don\'t make all field names lowercase (for R) default: lower all fieldnames')
 
-parser.add_argument('--join-field', metavar='fieldname_to_join_on', type=str, default=None, help='field needed to join across input files (e.g. the key)')
+#parser.add_argument('--print-duplicate-join-fields', action='store_const', const=True, default=False, help='don\'t suppress the printing of additional (duplicate) join fields from all the files, default is to only print one of them (from --smaller file)')
+
+parser.add_argument('--join-field', metavar='fieldname_to_join_on', type=str, help='field needed to join across input files (if the same) or for the smaller file (if different), required')
+parser.add_argument('--larger-field', metavar='fieldname_to_join_on', type=str, default=None, help='field needed to join the larger input file on (e.g. the key), defaults to the value for --smaller-key')
 
 args = parser.parse_args()
 
@@ -43,49 +46,86 @@ if args.join_field is None:
     sys.stderr.write("ERROR: argument to --join-field, terminating\n")
     exit(-1)
 
-key = args.join_field
+smaller_key = args.join_field
+larger_key = args.larger_field
+if larger_key is None:
+    larger_key = smaller_key
+
 smap = {}
-smaller_fields = None
+smaller_fields_deduped = []
+smaller_fields_set = set()
+smaller_idxs = []
 with open(args.smaller, newline='') as delimited_file:
     reader = csv.DictReader(delimited_file, restkey='EXTRAFIELDS', delimiter=args.smaller_sep)
-    smaller_fields = [field.replace('\n', ' ') for field in reader.fieldnames]
+    for (i,field) in enumerate(reader.fieldnames):
+        field_purged = field.replace('\n', ' ').replace(' ', '_')
+        #TODO make lower an option
+        field_purged = field_purged.lower()
+        if field_purged not in smaller_fields_set:
+            smaller_idxs.append(i)
+            smaller_fields_set.add(field_purged)
+            smaller_fields_deduped.append(field_purged)
     for row in reader:
-        if row[key] not in smap:
-            smap[row[key]] = []
-        row_ = args.output_sep.join([field.replace('\n',' ') for field in row.values()]) 
-        smap[row[key]].append(row_)
+        if row[smaller_key] not in smap:
+            smap[row[smaller_key]] = []
+        #still need original casing for matching reader's row fields
+        fields = []
+        for i in smaller_idxs:
+            field = row[reader.fieldnames[i]]
+            if field is not None:
+                field = field.replace('\n',' ')
+            else:
+                field = ""
+            fields.append(field)
+        row_ = args.output_sep.join(fields)
+        #use original key value for matching with larger file
+        smap[row[smaller_key]].append(row_)
 
-num_smaller_fields = len(smaller_fields)
+num_smaller_fields = len(smaller_fields_deduped)
 smaller_blanks_row = args.output_sep.join([""]*num_smaller_fields)
-
+    
 seen = set()
-larger_fields = None
+larger_fields = []
+larger_fields_deduped = []
+larger_fields_set = set()
+larger_idxs = []
 with open(args.larger, newline='') as delimited_file:
     reader = csv.DictReader(delimited_file, restkey='EXTRAFIELDS', delimiter=args.larger_sep)
-    larger_fields = reader.fieldnames
-    if not args.print_duplicate_join_fields:
-        larger_fields = [field.replace('\n', ' ') for field in larger_fields if field != key]
+    for (i,field) in enumerate(reader.fieldnames):
+        field_purged = field.replace('\n', ' ').replace(' ', '_')
+        #TODO make lower an option
+        field_purged = field_purged.lower()
+        if field_purged not in larger_fields_set and field_purged not in smaller_fields_set:
+            larger_idxs.append(i)
+            larger_fields_deduped.append(field_purged)
+            larger_fields_set.add(field_purged)
     #write out joined header
-    sys.stdout.write(args.output_sep.join(smaller_fields) + args.output_sep + args.output_sep.join(larger_fields) + '\n')
+    sys.stdout.write(args.output_sep.join(smaller_fields_deduped) + args.output_sep + args.output_sep.join(larger_fields_deduped) + '\n')
     for row in reader:
-        row_ = args.output_sep.join([row[k].replace('\n', ' ') for k in row.keys() if k != key])
-        if args.print_duplicate_join_fields:
-            row_ = args.output_sep.join([field.replace('\n', ' ') for field in row.values()])
-        if row[key] not in smap:
+        fields = []
+        for i in larger_idxs:
+            field = row[reader.fieldnames[i]]
+            if field is not None:
+                field = field.replace('\n',' ')
+            else:
+                field = ""
+            fields.append(field)
+        row_ = args.output_sep.join(fields)
+        #use original key value for matching with smaller file
+        if row[larger_key] not in smap:
             if args.larger_matching:
                 sys.stderr.write("WARNING: row in larger %s not in smaller %s: %s; skipping it\n" % (args.larger, args.smaller, row_))
             else:
                 sys.stderr.write("WARNING: row in larger %s not in smaller %s: %s; padding it\n" % (args.larger, args.smaller, row_))
                 sys.stdout.write(smaller_blanks_row + args.output_sep + row_ + '\n')
             continue
-        smap_rows = smap[row[key]]
+        smap_rows = smap[row[larger_key]]
         for smap_row in smap_rows:
             sys.stdout.write(smap_row + args.output_sep + row_ + '\n')
-        seen.add(row[key])
-
-
+        seen.add(row[larger_key])
+    
 #now output all rows in smaller file which didn't match anything in larger
-num_larger_fields = len(larger_fields)
+num_larger_fields = len(larger_fields_deduped)
 larger_blanks_row = args.output_sep.join([""]*num_larger_fields)
 
 for k in smap.keys():
