116ec8586e5202279be68f28a6ae5bd4006ba13e
btrx-cwilks <cwilks@rbneuro.com>
2021-02-10 18:39:55 +0000

    adding scripts to check gene and exon sums output

--- scripts/check_unifier_outputs.pl
+++ scripts/check_unifier_outputs.pl
@@ -2,12 +2,15 @@
 use strict;
 use warnings;
 
+#e.g. exon_sums_per_study/42/SRP044042/sra.exon_sums.SRP044042.G026.gz
+my $input_file_name = shift;
 my $counts_per_study_file = shift;
-#genes, exons, or junctions
-my $ftype = $shift;
+#gene, exon, or junction(?)
+my $ftype = shift;
 #e.g ${GENE_EXON_ANNOTATION_ROW_COUNTS}
 my $row_counts_file = shift;
 
+#load expected column (sample) counts
 my %counts_per_study;
 open(FIN,"<$counts_per_study_file");
 while(my $line = <FIN>) 
@@ -18,55 +21,82 @@ while(my $line = <FIN>)
 }
 close(FIN);
 
+#load expected row (gene or exon) counts
 my %counts_per_annotation;
 open(FIN,"<$row_counts_file");
 while(my $line = <FIN>) 
 {
     chomp($line);
-    my ($annot,$row_count)=split(/\t/,$line);
-    $counts_per_annotation{uc($annot)}=$row_count;
+    my ($atype,$annot,$row_count)=split(/\t/,$line);
+    #gene, exon, or junction
+    if($atype eq $ftype)
+    {
+        $counts_per_annotation{uc($annot)}=$row_count;
+    }
 }
 close(FIN);
 
+#$line=~/^[^\/]+\/..\/([^\/]+)\/.*\.gz$/;
+#$line=~/^[^\/]+\/..\/([^\/]+)\/([^\.]+)\.([^\.]+).([^\.]+).([^\.]+).gz
+my @path_segment = split(/\//,$input_file_name);
+my $dir = shift(@path_segment);
+#low order of study
+shift(@path_segment);
+my $study = shift(@path_segment);
+$study = uc($study);
+my $base = pop(@path_segment);
 my $ERROR=undef;
-while(my $line = <STDIN>)
+if(!defined($counts_per_study{$study}))
 {
-    chomp($line);
-    #e.g. exon_sums_per_study/42/SRP044042/sra.exon_sums.SRP044042.G026.gz
-    #$line=~/^[^\/]+\/..\/([^\/]+)\/.*\.gz$/;
-    #$line=~/^[^\/]+\/..\/([^\/]+)\/([^\.]+)\.([^\.]+).([^\.]+).([^\.]+).gz
-    my @path_segment = split(/\//,$line);
-    my $dir = shift(@path_segment);
-    #low order of study
-    shift(@path_segment);
-    my $study = shift(@path_segment);
-    $study = uc($study);
-    my $base = pop(@path_segment);
-    if(!defined($counts_per_study{$study}))
+    print STDERR "ERROR\t$study not in $counts_per_study_file\n";
+    $ERROR=1;
+}
+
+my $study_count = $counts_per_study{$study};
+my $num_rows_expected;
+if($ftype=~/(exon)|(gene)/i)
+{
+    my ($source,$type,$study,$annot)=split(/\./,$base);
+    $num_rows_expected = $counts_per_annotation{uc($annot)};
+}
+
+my $last_line;
+my $num_rows=0;
+while($last_line = <STDIN>)
+{
+    $num_rows++;
+    chomp($last_line);
+    #if there are empty strings (blanks) in one or more columns in the last line, this will pick it up
+    my @cols = split(/\t/,$last_line);
+    my $num_cols = scalar(@cols);
+    ##check number of columns (samples) in the *last line* of the file
+    #my $num_cols=`zcat $line | tail -n1 | tr \\t \\n | wc -l`
+    #subtract one for first column (not a sample for genes/exons)
+    $num_cols--;
+    if($study_count != $num_cols)
     {
-        print STDERR "ERROR\t$study not in $counts_per_study_file\n";
+        print STDERR "ERROR\texpected column count:$study_count != column count:$num_cols\tline#$num_rows\t$last_line\n";
         $ERROR=1;
     }
-    my $study_count = $counts_per_study{$study};
-    if($ftype=~/(exons)|(genes)/i)
-    {
-        my ($source,$type,$study,$annot)=split(/\./,$base);
-        my $num_cols=`zcat $line | tail -n1 | tr \\t \\n | wc -l`
-        #subtract one for first column (not a sample for genes/exons)
-        $num_cols--;
-        if($study_count != $num_cols)
-        {
-            print STDERR "ERROR\texpected column count:$study_count != column count:$num_cols in $line\n";
-            $ERROR=1;
-        }
-        my $num_rows=`zcat $line | cut -f 1 | wc -l`;
-        #subtract one for first row (header)
-        $num_rows--;
-        if($num_rows_expected != $num_rows)
-        {
-            print STDERR "ERROR\texpected row count:$num_rows_expected != row count:$num_rows in $line\n";
-            $ERROR=1;
-        }
-    }
 }
-print "CHECKS_DONE\n";
+
+##check number of rows (lines) in the file
+#my $num_rows=`zcat $line | cut -f 1 | wc -l`;
+#subtract one for first row (header)
+$num_rows--;
+if($num_rows_expected != $num_rows)
+{
+    print STDERR "ERROR\texpected row count:$num_rows_expected != row count:$num_rows\n";
+    $ERROR=1;
+}
+
+print $input_file_name."\t";
+if(!defined($ERROR))
+{
+    print "NUM_ROWS_COLUMNS_AND_NO_BLANKS_CHECKS_PASSED\n";
+}
+else
+{
+    print "CHECKS_FAILED\n";
+    exit -1;
+}
---
+++ scripts/run_gene_exon_sum_checks.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#smoke tests of counts of number of studies, rows (genes/exons), and columns (samples) across gene and exon counts files
+set -exo pipefail
+
+dir=$(dirname $0)
+
+#env vars should be already defined:
+#SAMPLE_ID_MANIFEST
+#num_studies
+#LIST_OF_ANNOTATIONS
+
+#need to do smoke tests for proper outputs based on:
+#get number of samples per-study
+export LC_ALL=C
+cut -f 1 $SAMPLE_ID_MANIFEST | sort | uniq -c | tr -s " " \\t | cut -f 2,3 > ${SAMPLE_ID_MANIFEST}.num_samples_per_study.tsv
+
+num_files=$(echo "$LIST_OF_ANNOTATIONS" | tr "," \\n | wc -l)
+#2) number of files * number of studies
+num_expected=$(( num_studies * num_files ))
+num_gene_files=$(find gene_sums_per_study -name "*.gz" -size +0c | wc -l)
+if [[ $num_expected -ne $num_gene_files ]]; then
+    echo "FAILURE running unify, unexpected # of gene sum files: $num_gene_files vs. $num_expected (expected)"
+    exit -1
+fi
+num_exon_files=$(find exon_sums_per_study -name "*.gz" -size +0c | wc -l)
+if [[ $num_expected -ne $num_exon_files ]]; then
+    echo "FAILURE running unify, unexpected # of exon sum files: $num_exon_files vs. $num_expected (expected)"
+    exit -1
+fi
+
+#check 1) row and 2) column (sample) counts per study file for genes
+for f in `find gene_sums_per_study -name "*.gz" -size +0c` ; do pcat $f | fgrep -v "##" | perl $dir/check_unifier_outputs.pl $f ${SAMPLE_ID_MANIFEST}.num_samples_per_study.tsv gene ${GENE_EXON_ANNOTATION_ROW_COUNTS_FILE} ; done > gene_checks.tsv
+
+num_passed=$(fgrep "NUM_ROWS_COLUMNS_AND_NO_BLANKS_CHECKS_PASSED" gene_checks.tsv | wc -l)
+if [[ $num_passed -ne $num_files ]]; then
+    cat <(echo "FAILURE running unify, failed gene sums row/column checks:") gene_checks.tsv
+    exit -1
+fi
+
+#check 1) row and 2) column (sample) counts per study file for exons
+for f in `find exon_sums_per_study -name "*.gz" -size +0c` ; do pcat $f | fgrep -v "##" | perl $dir/check_unifier_outputs.pl $f ${SAMPLE_ID_MANIFEST}.num_samples_per_study.tsv exon ${GENE_EXON_ANNOTATION_ROW_COUNTS_FILE} ; done > exon_checks.tsv
+
+num_passed=$(fgrep "NUM_ROWS_COLUMNS_AND_NO_BLANKS_CHECKS_PASSED" exon_checks.tsv | wc -l)
+if [[ $num_passed -ne $num_files ]]; then
+    cat <(echo "FAILURE running unify, failed exon sums row/column checks:") exon_checks.tsv
+    exit -1
+fi
