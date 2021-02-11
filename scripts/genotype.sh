8accc65ab0392630c16fbaeac4cd1cfeb41bc0be
btrx-cwilks <cwilks@rbneuro.com>
2021-02-11 22:01:41 +0000

    switch to bcftools for initial genotyping step for speed

--- scripts/genotype.sh
+++ scripts/genotype.sh
@@ -5,8 +5,13 @@ set -exo pipefail
 BAM=$1
 SNPS=$2
 GENOME_FA=$3
+THREADS=$4
 
-samtools mpileup -l $SNPS -AB -q0 -Q13 -d1000000 -uf $GENOME_FA ${BAM} -o ${BAM}.snps_temp
-#~/time $dir/bcftools mpileup --threads 4 -R $snps_file -AB -q0 -Q13 -d1000000 -f $genome_fa -O v -o ${BAM}.snps_temp ${BAM}
+if [[ ! -e ${BAM}.bai ]]; then
+    samtools index -@ $THREADS $BAM
+fi
+
+#samtools mpileup -l $SNPS -AB -q0 -Q13 -d1000000 -uf $GENOME_FA ${BAM} -o ${BAM}.snps_temp
+bcftools mpileup --threads $THREADS -R $SNPS -AB -q0 -Q13 -d1000000 -f $GENOME_FA -O v -o ${BAM}.snps_temp ${BAM}
 bcftools call -mv -Oz ${BAM}.snps_temp > ${BAM}.genotyped.tsv.gz
 rm -f ${BAM}.snps_temp
