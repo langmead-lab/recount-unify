570add26ad79674981c27157da3d044cdcd0cf18
btrx-cwilks <cwilks@rbneuro.com>
2021-02-11 18:52:35 +0000

    added correct expected gene/exon sum counts to compare with in smoke tests; remove unecessary popds

--- scripts/run_gene_exon_sum_checks.sh
+++ scripts/run_gene_exon_sum_checks.sh
@@ -8,6 +8,7 @@ dir=$(dirname $0)
 #SAMPLE_ID_MANIFEST
 #num_studies
 #LIST_OF_ANNOTATIONS
+#GENE_EXON_ANNOTATION_ROW_COUNTS_FILE
 
 #need to do smoke tests for proper outputs based on:
 #get number of samples per-study
@@ -32,7 +33,7 @@ fi
 for f in `find gene_sums_per_study -name "*.gz" -size +0c` ; do pcat $f | fgrep -v "##" | perl $dir/check_unifier_outputs.pl $f ${SAMPLE_ID_MANIFEST}.num_samples_per_study.tsv gene ${GENE_EXON_ANNOTATION_ROW_COUNTS_FILE} ; done > gene_checks.tsv
 
 num_passed=$(fgrep "NUM_ROWS_COLUMNS_AND_NO_BLANKS_CHECKS_PASSED" gene_checks.tsv | wc -l)
-if [[ $num_passed -ne $num_files ]]; then
+if [[ $num_passed -ne $num_gene_files ]]; then
     cat <(echo "FAILURE running unify, failed gene sums row/column checks:") gene_checks.tsv
     exit -1
 fi
@@ -41,7 +42,7 @@ fi
 for f in `find exon_sums_per_study -name "*.gz" -size +0c` ; do pcat $f | fgrep -v "##" | perl $dir/check_unifier_outputs.pl $f ${SAMPLE_ID_MANIFEST}.num_samples_per_study.tsv exon ${GENE_EXON_ANNOTATION_ROW_COUNTS_FILE} ; done > exon_checks.tsv
 
 num_passed=$(fgrep "NUM_ROWS_COLUMNS_AND_NO_BLANKS_CHECKS_PASSED" exon_checks.tsv | wc -l)
-if [[ $num_passed -ne $num_files ]]; then
+if [[ $num_passed -ne $num_exon_files ]]; then
     cat <(echo "FAILURE running unify, failed exon sums row/column checks:") exon_checks.tsv
     exit -1
 fi
