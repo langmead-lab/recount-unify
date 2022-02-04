d0119ed1fbd4f9f49a219207d6d718de941ddb5a
Christopher Wilks <cwilks3@jhu.edu>
2022-02-04 16:44:20 -0500

    updated with full pipeline (other than copy back)

--- rejoin/fix/coverage_for_study.sh
+++ rejoin/fix/coverage_for_study.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env
 set -exo pipefail
 dir=$(dirname $0)
-ROOT=
-GP=$ROOT/release/human/data_sources/sra/gene_sums
-BP=$ROOT/release/human/data_sources/sra/base_sums
+orgn="human"
+ddir=$PWD/../
+ROOT=#SET THIS#
 rejoin=$ROOT/recount-unify/rejoin/rejoin.postfix
-#bed=$dir/disjoint2exons2genes.fix.bed.disjoint_exons.sorted
-bed=$dir/recount3.exons.bed
-orgn=human
+bed=$ddir/disjoint2exons2genes.fix.bed.disjoint_exons.sorted
+sanity_check=$ddir/good_genes_sanity_check.txt
+#bed=$dir/recount3.exons.bed
 
 study=$1
 src=$2
-annot=$3
 
-if [[ -z $annot ]]; then
-    annot="G026"
-fi
+GP=$ROOT/release/${orgn}/data_sources/${src}/gene_sums
+BP=$ROOT/release/${orgn}/data_sources/${src}/base_sums
 
 mkdir -p $study
 pushd $study
@@ -24,35 +22,56 @@ bed_short=$(basename $bed)
 
 lo=${study: -2}
 
+for annot in G026 G029 R109 F006; do
 set +o pipefail
-pcat $GP/$lo/$study/${src}.gene_sums.${study}.${annot}.gz | head -3 | tail -n1 | tr $'\t' $'\n' | tail -n+2 > samples
+    pcat $GP/$lo/$study/${src}.gene_sums.${study}.${annot}.gz | head -3 | tail -n1 | tr $'\t' $'\n' | tail -n+2 > ${annot}.samples
 set -o pipefail
-num_samples=$(cat samples | wc -l)
+done
+
+num_samples=$(cat G026.samples | wc -l)
 BPP=$BP/$lo/$study
 if [[ -z $SKIP_MD ]]; then
     echo -n "" > list_of_output.files
     echo -n "" > header.1
     echo -n "" > md.jobs
     n=0
-    for sample in `cat samples`; do
+    for sample in `cat G026.samples`; do
         n=$((n + 1))
         slo=${sample: -2}
-        bw=$BPP/$slo/${src}.base_sums.${study}_${sample}.ALL.bw
+        bw=$BPP/$slo/$sample/${src}.base_sums.${study}_${sample}.ALL.bw
         echo "/usr/bin/time -v /bin/bash -x $dir/bigwig_coverage.sh $bw $bed ${sample}.md > ${sample}.md.run 2>&1" >> md.jobs
         echo "${sample}.md.notsummed " >> list_of_output.files
         echo -n "	$n" >> header.1
     done
+    echo "" >> header.1
+    /usr/bin/time -v parallel -j20 < md.jobs > md.jobs.run 2>&1
 fi
-
-/usr/bin/time -v parallel -j20 < md.jobs > md.jobs.run 2>&1
 
 set +o pipefail
 pcat $GP/${src}.gene_sums.${study}.${annot}.gz | head -3 > ${src}.gene_sums.${study}.${annot}
 set -o pipefail
 list=$(cat list_of_output.files)
 cat <(echo -n $'gene\tstart\tend\tname\tscore\tstrand') <(cat header.1) > ${study}.genes.pasted.tsv
+#alternate approach to pasting to get around number of files open/too long command line
+#from https://unix.stackexchange.com/questions/205642/combining-large-amount-of-files
+#echo -n "" > ${study}.genes.pasted.tsv_
+#set +e
+#for f in $list; do cat ${study}.genes.pasted.tsv_ | paste - $f >temp; cp temp ${study}.genes.pasted.tsv_; done; rm temp
+#cat ${study}.genes.pasted.tsv_ >> ${study}.genes.pasted.tsv ; rm ${study}.genes.pasted.tsv_
+#set -e
 paste $bed $list >> ${study}.genes.pasted.tsv
 
-$rejoin -a $dir/disjoint2exons2genes.fix.sorted.bed -d ${study}.genes.pasted.tsv -s $num_samples -p gene -h
+$rejoin -a $ddir/disjoint2exons2genes.fix.sorted.bed -d ${study}.genes.pasted.tsv -s $num_samples -p gene -h
+
+for annot in G026 G029 R109 F006; do
+    fgrep ".${annot}	" gene.counts | cut -f 1,7- > gene.counts.${annot}
+    #then do update python3 script of original gene counts
+    pcat $GP/$lo/$study/${src}.gene_sums.${study}.${annot}.gz | python3 $dir/update_counts.py <(cat gene.counts.${annot} | sed 's#\.'$annot'##') <(fgrep ".${annot}" $sanity_check | sed 's#\.'$annot'##' | sed 's#\t#|#g') 2> ${src}.gene_sums.${study}.${annot}.gz.update | pigz --fast -p2 > ${src}.gene_sums.${study}.${annot}.gz
+    set +e
+    diff <(pcat ${src}.gene_sums.${study}.${annot}.gz) <(pcat $GP/$lo/$study/${src}.gene_sums.${study}.${annot}.gz) > ${annot}.diff
+    set -e
+    fgrep "<" ${annot}.diff | wc -l > ${annot}.diff.check
+    fgrep ">" ${annot}.diff | wc -l >> ${annot}.diff.check
+done
 
 popd
---
+++ rejoin/fix/update_counts.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import sys
+
+#e.g. gene.counts.G026
+#first column is gene_id, rest are sums
+#NO HEADER
+updated_counts_for_annotationF=sys.argv[1]
+sanity_check_geneF=sys.argv[2]
+#sys.stderr.write(sanity_check_gene+"\n")
+#only expect 1 line
+with open(sanity_check_geneF,"r") as fin0:
+    for line in fin0:
+        (chrm,start,end,strand,sanity_gene)=line.rstrip().split('|')
+
+new_counts_map = {}
+with open(updated_counts_for_annotationF,"r") as fin:
+    for line in fin:
+        line = line.rstrip()
+        gene_id_pos = line.find("\t")
+        if gene_id_pos == -1:
+            sys.stderr.write("bad row, no tabs, terminating early!")
+            sys.exit(-1)
+        gene_id = line[:gene_id_pos]
+        new_counts_map[gene_id] = line[gene_id_pos:]
+
+#has 3 header lines
+idx=0
+for line in sys.stdin:
+    line_original = line
+    idx += 1
+    if idx < 4:
+        sys.stdout.write(line)
+        continue
+    line = line.rstrip()
+    gene_id_pos = line.find("\t")
+    if gene_id_pos == -1:
+        sys.stderr.write("bad row, no tabs, terminating early!")
+        sys.exit(-1)
+    current_counts = line[gene_id_pos:]
+    gene_id = line[:gene_id_pos]
+    if gene_id not in new_counts_map:
+        sys.stdout.write(line_original)
+        continue
+    new_counts = new_counts_map[gene_id]
+    if new_counts == current_counts:
+        sys.stderr.write("gene_counts_match\t"+gene_id+"\n")
+        sys.stdout.write(line_original)
+    else:
+        if gene_id == sanity_gene:
+            sys.stderr.write("sanity_check_gene_counts_changed\t"+sanity_gene+"\n")
+            sys.exit(-1)
+        sys.stderr.write("gene_counts_no_match\t"+gene_id+"\n")
+        sys.stdout.write(gene_id+"\t"+new_counts+"\n")
