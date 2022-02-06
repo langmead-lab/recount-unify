34f94ddbf619145e0adc65c9599d381c0ad29f7b
Christopher Wilks <cwilks3@jhu.edu>
2022-02-06 16:38:55 -0500

    added 3 checks, removed cruft

--- rejoin/fix/coverage_for_study.sh
+++ rejoin/fix/coverage_for_study.sh
@@ -7,6 +7,7 @@ ddir=$PWD/../
 ROOT=/datascope/recount03
 rejoin=$ROOT/recount-unify/rejoin/rejoin.postfix
 bed=$ddir/disjoint2exons2genes.fix.bed.disjoint_exons.sorted
+num_bed_lines=146725
 sanity_check=$ddir/good_genes_sanity_check.txt
 #bed=$dir/recount3.exons.bed
 threads=20
@@ -59,6 +60,13 @@ if [[ -z $SKIP_MD ]]; then
     /usr/bin/time -v parallel -j$threads < md.jobs > md.jobs.run 2>&1
 fi
 
+wc -l *.md.notsummed  > line_checks
+num_good_files=$(fgrep "$num_bed_lines" line_checks | wc -l)
+if [[ $num_good_files -ne $num_samples ]]; then
+    echo "not all samples have required number of lines, terminating early!"
+    exit -1
+fi 
+
 set +o pipefail
 pcat $GP/${src}.gene_sums.${study}.${annot}.gz | head -3 > ${src}.gene_sums.${study}.${annot}
 set -o pipefail
@@ -72,14 +80,6 @@ list=$(cat pasted.outputs)
 cat <(echo -n $'gene\tstart\tend\tname\tscore\tstrand') <(cat header.1) > ${study}.genes.pasted.tsv
 paste $bed $list >> ${study}.genes.pasted.tsv
 
-#alternate approach to pasting to get around number of files open/too long command line
-#from https://unix.stackexchange.com/questions/205642/combining-large-amount-of-files
-#cat $bed > ${study}.genes.pasted.tsv_
-#set +e
-#for f in $list; do cat ${study}.genes.pasted.tsv_ | paste - $f >temp; cp temp ${study}.genes.pasted.tsv_; done; rm temp
-#cat ${study}.genes.pasted.tsv_ >> ${study}.genes.pasted.tsv ; rm ${study}.genes.pasted.tsv_
-#set -e
-
 $rejoin -a $ddir/disjoint2exons2genes.fix.sorted.bed -d ${study}.genes.pasted.tsv -s $num_samples -p gene -h
 
 for annot in G026 G029 R109 F006; do
@@ -89,8 +89,20 @@ for annot in G026 G029 R109 F006; do
     set +e
     diff <(pcat ${src}.gene_sums.${study}.${annot}.gz) <(pcat $GP/$lo/$study/${src}.gene_sums.${study}.${annot}.gz) > ${annot}.diff
     set -e
-    fgrep "<" ${annot}.diff | wc -l > ${annot}.diff.check
+    total_num_fields=$(fgrep "<" ${annot}.diff | cut -f2- | tr $'\t' $'\n' | wc -l)
+    num_diff_lines=$(fgrep "<" ${annot}.diff | wc -l)
+    expected_total_num_fields=$((num_samples * num_diff_lines)) 
+    if [[ $total_num_fields -ne $expected_total_num_fields ]]; then
+        echo "bad number of fields in new part of diff, terminating early!"
+        exit -1
+    fi
+    echo "$num_diff_lines" > ${annot}.diff.check
     fgrep ">" ${annot}.diff | wc -l >> ${annot}.diff.check
 done
+#check that we get expected number of diffs and that they agree between the two files
+cat G026.diff.check | tr $'\n' ':' | perl -ne 'chomp; ($n1,$n2)=split(/:/,$_,-1); die "bad diff count $n1 for G026\n" if($n1 != $n2 || $n1 > 1797);'
+cat G029.diff.check | tr $'\n' ':' | perl -ne 'chomp; ($n1,$n2)=split(/:/,$_,-1); die "bad diff count $n1 for G029\n" if($n1 != $n2 || $n1 > 1364);'
+cat F006.diff.check | tr $'\n' ':' | perl -ne 'chomp; ($n1,$n2)=split(/:/,$_,-1); die "bad diff count $n1 for F006\n" if($n1 != $n2 || $n1 > 74);'
+cat R109.diff.check | tr $'\n' ':' | perl -ne 'chomp; ($n1,$n2)=split(/:/,$_,-1); die "bad diff count $n1 for R109\n" if($n1 != $n2 || $n1 > 2589);'
 
 popd
