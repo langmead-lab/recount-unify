c5e43694f9de387476ecf751f36f0ffb240816f0
Christopher Wilks <cwilks3@jhu.edu>
2022-02-12 13:29:22 -0500

    adding paste-per-study-group script

---
+++ rejoin/fix/paste_snaptron_counts_per_study.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -exo pipefail 
+annot="G026"
+
+study_listF=$1
+
+sdir="DONES/"$(echo "$study_listF" | sed 's#studies##')
+
+pushd $sdir
+
+pcmd="paste "
+for s in `cat ../../$study_listF`; do
+    pcmd="${pcmd} $s/${annot}.snaptron"
+done 
+$pcmd | sed 's#\t##g' > ../../${study_listF}.snaptron.pasted.1
+paste /datascope/recount03/rejoin_fix/disjoint2exons2genes.fix.sorted.bed.G026_genes.tabs.sorted ../../${study_listF}.snaptron.pasted.1 > ../../${study_listF}.snaptron.pasted
+
+popd
