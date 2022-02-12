6498439e24bae0effab0d13a13b3b305da04f4de
Christopher Wilks <cwilks3@jhu.edu>
2022-02-12 13:29:35 -0500

    support gtex and tcga

--- rejoin/fix/get_updated_snaptron_counts_per_study.sh
+++ rejoin/fix/get_updated_snaptron_counts_per_study.sh
@@ -7,9 +7,20 @@ genes="$ddir/disjoint2exons2genes.fix.sorted.bed.${annot}_genes.tabs"
 ids="$ddir/recount3.sra.samples.tsv.cut"
 
 study=$1
+
+cwd=$(basename $PWD)
+updated_gene_sums=sra.gene_sums.${study}.${annot}.gz
+if [[ $cwd == "tcga." ]]; then
+    ids="$ddir/tcgav2.samples.tsv.cut"
+    updated_gene_sums=tcga.gene_sums.${study}.${annot}.gz
+fi
+if [[ $cwd == "gtex." ]]; then
+    ids="$ddir/gtexv2.samples.tsv.cut"
+    updated_gene_sums=gtex.gene_sums.${study}.${annot}.gz
+fi
+    
 pushd $study
 
-updated_gene_sums=sra.gene_sums.${study}.${annot}.gz
 set +o pipefail
 pcat $updated_gene_sums | tail -n+3 | head -1 | cut -f 2- | tr '\t' '\n' > ${updated_gene_sums}.samples
 set -o pipefail
