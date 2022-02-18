bd48aaf3f5c9b4bd310467f871cfd74c355c94c1
Christopher Wilks <cwilks3@jhu.edu>
2022-02-18 10:37:23 -0500

    remove per study group gene names pasting to support full rebuild

--- rejoin/fix/paste_snaptron_counts_per_study.sh
+++ rejoin/fix/paste_snaptron_counts_per_study.sh
@@ -10,9 +10,10 @@ pushd $sdir
 
 pcmd="paste "
 for s in `cat ../../$study_listF`; do
-    pcmd="${pcmd} $s/${annot}.snaptron"
+    #pcmd="${pcmd} $s/${annot}.snaptron"
+    pcmd="${pcmd} $s/${annot}.snaptron_full"
 done 
 $pcmd | sed 's#\t##g' > ../../${study_listF}.snaptron.pasted.1
-paste /datascope/recount03/rejoin_fix/disjoint2exons2genes.fix.sorted.bed.G026_genes.tabs.sorted ../../${study_listF}.snaptron.pasted.1 > ../../${study_listF}.snaptron.pasted
+#paste /datascope/recount03/rejoin_fix/disjoint2exons2genes.fix.sorted.bed.G026_genes.tabs.sorted ../../${study_listF}.snaptron.pasted.1 > ../../${study_listF}.snaptron.pasted
 
 popd
