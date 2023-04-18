1248f06b32fc22535b72c80f30281413f413489d
btrx-cwilks <cwilks@rbneuro.com>
2023-04-18 19:32:22 +0000

    adding additional runner scripts for large scale runs

---
+++ run_workers.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#Main multi-Unifier runner script:
+#1) formats and mounts SSDs
+#2) downloads Monorail Unifier references to SSD
+#3) stars N Unifier workers in parallel
+#Assumes the following ENV vars are set:
+#a) REF (hg38 or grcm38, required)
+#b) NUM_WORKERS (number of concurrent workers to start, default: 16)
+#c) NUM_CORES (maximum number of CPUs to use per worker, default: 8)
+#d) SSD_MIN_SIZE (minimum size of local SSDs, default: 600GBs)
+set -exo pipefail
+dir=$(dirname $0)
+#export ROOT=/shared-data/research/genomics/software/monorail
+if [[ -z $REF ]]; then
+    echo "no REF set, terminating early!"
+    exit -1
+fi
+if [[ -z $NUM_WORKERS ]]; then
+    #export NUM_WORKERS=16
+    export NUM_WORKERS=2
+fi
+if [[ -z $NUM_CORES ]]; then
+    export NUM_CORES=8
+fi
+if [[ -z $SSD_MIN_SIZE ]]; then
+    export SSD_MIN_SIZE=600000000000
+fi
+
+df=$(df | fgrep "/work1")
+#1) format and mount SSDs (but skip root)
+if [[ -z $df ]]; then
+    i=1
+    for d in `lsblk -b | egrep -e '^nvme' | fgrep -v nvme0n1 | perl -ne '@f=split(/\s+/,$_,-1); next if($f[3] < '$SSD_MIN_SIZE'); print $f[0]."\n";'`; do sudo mkfs -q -t ext4 /dev/$d ; sudo mkdir -p /work${i} ; sudo mount /dev/$d /work${i}/ ; sudo chown -R ubuntu /work${i} ; sudo chmod -R a+rw /work${i} ; i=$((i + 1)); done
+fi
+
+#2) download Unifier references to SSD
+if [[ ! -d /work1/ref ]]; then
+    mkdir /work1/ref
+    pushd /work1/ref
+    /usr/bin/time -v $dir/../get_unify_refs.sh $REF > get_unify_refs.sh.${REF}.run 2>&1
+    popd
+fi 
+
+#3) start N concurrent Unifier workers
+export REF_DIR=/work1/ref
+pushd /work1
+mkdir -p /work1/runs
+echo -n "" > worker.jobs
+for i in $( seq 1 $NUM_WORKERS ); do echo "/usr/bin/time -v /bin/bash -x $dir/worker.sh > /work1/runs/w${i}.run 2>&1" >> worker.jobs ; done
+cat worker.jobs
+#/usr/bin/time -v parallel -j${NUM_WORKERS} < worker.jobs > worker.jobs.run 2>&1
---
+++ worker.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+#Main single-Unifier runner script:
+#in a loop:
+#1) checks for job on unifier SQS (one study)
+#2) dequeues job and gets date_time stamp at time of dequeue
+#3) copies down from S3 all results and setups up local FS links
+#4) calls workflow.bash.recount_jxns_only (recount3 only) on study
+#5) on success, copies results into final S3 location (Open Data) and either removes or moves the pump results (to avoid re-queuing)
+#6) regardless of status of 4/5, removes results and temporary files from local SSDs to keep usage down
+#7) repeat
+#Assumes the following ENV vars are set:
+#a) REF (hg38 or grcm38, required)
+#b) Q (queue, usually SQS URL) to retrieve jobs from (1 per study to unify, default: monorail_batch on SQS)
+#c) DOCKER_IMAGE (docker image of Unifier to use, default: 1.1.1)
+#d) REF_DIR (references directory, default: /work1/ref)
+#e) NUM_CORES (maximum number of CPUs to use per worker, default: 8)
+#f) OUTPUT_DIR_GLOBAL (where to write the unifier outputs temporarily, before copying back to S3, default: /work1/unifier/$study)
+#g) S3_OUTPUT (where to upload the unifier results to on S3, default: s3://neuro-datalake-ds/research/genomics/raw_data/monorail_temp_input/unifier_outputs/)
+set -exo pipefail
+dir=$(dirname $0)
+
+if [[ -z $REF ]]; then
+    echo "no REF set, terminating early!"
+    exit -1
+fi
+if [[ -z $Q ]]; then
+    export Q="https://sqs.us-east-1.amazonaws.com/315553526860/monorail_batch_unify"
+fi
+if [[ -z $DOCKER_IMAGE ]]; then
+    export DOCKER_IMAGE="315553526860.dkr.ecr.us-east-1.amazonaws.com/recount-unify-aarch64:1.1.1"
+fi
+if [[ -z $REF_DIR ]]; then
+    export REF_DIR=/work1/ref
+fi
+if [[ -z $NUM_CORES ]]; then
+    export NUM_CORES=8
+fi
+if [[ -z $OUTPUT_DIR_GLOBAL ]]; then
+    export OUTPUT_DIR_GLOBAL=/work1/unifier
+fi
+if [[ -z $S3_OUTPUT ]]; then
+    export S3_OUTPUT="s3://neuro-datalake-ds/research/genomics/raw_data/monorail_temp_input/unifier_outputs"
+fi
+
+#1) check for new studies on the queue
+msg_json=$(aws sqs receive-message --queue-url $Q)
+while [[ -n $msg_json ]]; do
+    set +eo pipefail
+    handle=$(echo "$msg_json" | fgrep '"ReceiptHandle":' | cut -d'"' -f 4)
+    #e.g. s3://neuro-datalake-ds/research/genomics/raw_data/monorail_temp_input/pump_outputs/10/SRP277410
+    study_s3=$(echo "$msg_json" | fgrep '"Body":' | cut -d'"' -f 4)
+    set -eo pipefail
+    if [[ -z $handle || -z $study_s3 ]]; then
+        echo "ERROR: didn't find either a handle or a study in SQS message: $msg_json.  skipping"
+        aws sqs delete-message --queue-url $queue --receipt-handle $handle
+        msg_json=$(aws sqs receive-message --queue-url $queue)
+        continue
+    fi
+    date=$(date +%Y%m%d_%s)
+    study=$(basename $study_s3)
+    lo=${study: -2}
+    export OUTPUT_DIR=$OUTPUT_DIR_GLOBAL/$study.${date}
+    mkdir -p $OUTPUT_DIR
+    pushd $OUTPUT_DIR
+    #TODO: start a unifier job on the study
+    #2) download from S3 pump outputs for study
+    #/usr/bin/time -v aws s3 cp --recursive $study_s3 $study/
+    echo -n "" > ${study}.s3dnload.jobs
+    echo $'study_id\tsample_id' > samples4study.tsv
+    mkdir -p runs
+    mkdir -p pump
+    mkdir -p unify
+    for s in `aws s3 ls $sudy_s3/ | tr -s " " $'\t' | cut -f 4`; do
+        sample=$(basename $s)
+        echo "$study	$sample" >> samples4study.tsv
+        echo "/usr/bin/time -v aws s3 cp --recursive $study_s3/$sample/ ./$sample/ > ../runs/${sample}.s3dnload 2>&1" >> ${study}.s3dnload.jobs
+    done
+    pushd pump
+    /usr/bin/time -v parallel -j${NUM_CORES} < ../${study}.s3dnload.jobs > ../${study}.s3dnload.jobs.run 2>&1
+    popd
+    num_samples=$(cat ${study}.s3dnload.jobs | wc -l)
+    num_downloaded=$(fgrep "Exit " runs/*.s3dnload | fgrep 'Exit status: 0' | wc -l)
+    if [[ $num_downloaded -ne $num_samples ]]; then
+        echo "not all were able to download, skipping study $study"
+        msg_json=$(aws sqs receive-message --queue-url $queue)
+        continue
+    fi
+    #3) Run Unifier
+    #TODO: double check params
+    /usr/bin/time -v /bin/bash -x $dir/run_recount_unify_within_container.sh $REF $REF_DIR `pwd`/unifier `pwd`/pump `pwd`/samples4study.tsv $NUM_CORES > run_recount_unify_within_container.sh.run 2>&1
+    if [[ ! egrep -e '^SUCCESS$' run_recount_unify_within_container.sh.run ]]; then
+        echo "unifier failed for $study, skipping"
+        msg_json=$(aws sqs receive-message --queue-url $queue)
+        continue
+    fi
+    #4) copy unifier results back to S3
+    /usr/bin/time -v aws s3 cp --recursive `pwd`/unifier/ $S3_OUTPUT/$lo/$study.${date}/ > s3upload.run 2>&1
+    #get next message repeat
+    aws sqs delete-message --queue-url $queue --receipt-handle $handle
+    msg_json=$(aws sqs receive-message --queue-url $queue)
+done
