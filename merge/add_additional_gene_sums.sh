bf99ca6dd9429cb1d95c7450366543762e10ffbf
btrx-cwilks <cwilks@rbneuro.com>
2023-05-01 22:12:15 +0000

    adding an overall script for unifying additional annotation

--- merge/add_additional_gene_sums.sh
+++ merge/add_additional_gene_sums.sh
@@ -1,23 +1,80 @@
-#!/usr/bin/env bash
+#!/usr/bin/env
 set -exo pipefail
-#sum the bamcount (or megadepth) quantification of an additional split (non-overlapping) exon annotation (e.g. gencode v43)
-#per-sample, at the gene level (summing split exons into whole gene sums)
-#assumes both input files are the same order (so they gene_ids line up with the split-exon sums)
-OUTPUT_PATH="additional_gene_sums"
-#should be list of gene_id's matching # and order of additionall split-exon annotation BED used in Pump pipeline (in bamcount/megadepth), no header, single column (line can have multiple gene_ids separated by a "," for a single split exon)
-#e.g. /shared-data/research/genomics/datasets/recount3/prep4refresh/annotation/human/split_exonsv43.bed.gene_ids
-gene_idsF=$1
-#zst'd compressed file of 4 columns (chr,start,end,bp_sum), no header
-#e.g. DRR001175!DRP000425!hg38!sra.all1.annotation.tsv.zst
-split_exons_sumsF=$2
-output_path=$3
-if [[ -z $output_path ]]; then
-    output_path=$OUTPUT_PATH
-fi
-sample=$(basename $split_exons_sumsF)
-sample=$(echo "$sample" | cut -d'!' -f 1)
+sdir=$(dirname $0)
 
-if [[ ! -d $output_path ]]; then
-    mkdir -p $output_path
+#e.g. G043
+annotation=$1
+gene_idsF=$2
+exon_idsF=$3
+#e.g. gene_sums_per_study/<study_lo>/<study>/sra.gene_sums.DRP000425.G043.gz
+output_file=$4
+#e.g. DRP000425
+study=$5
+#e.g. hg38
+ref=$6
+#e.g. sra
+src=$7
+
+#optional
+threads=$8
+
+if [[ -z $threads ]]; then
+    threads=8
 fi
-/usr/bin/time -v paste $gene_idsF <(zstd -cd $split_exons_sumsF | cut -f 4) | perl -ne 'chomp; $f=$_; ($g,$b)=split(/\t/,$f,-1); @g=split(/,/,$g,-1); for $g0 (@g) { $h{$g0}+=$b; } END { for $g (sort { $a cmp $b } keys %h) { print "$g\t".$h{$g}."\n"; }}' > $output_path/${sample}.summed
+
+#assumes we're running as part of the Unifier
+echo -n "" > additional_gene_sums.files
+echo -n "" > additional_exon_sums.files
+for d in `fgrep "/" links/attempts.moved.complete | fgrep -f "$study"`; do
+    #e.g. /container-mounts/input/10/SRP277410/79/SRR12449879/SRR12449879_in3_att0
+    sample=$(basename $(dirname $f))
+    #DRR001175!DRP000425!hg38!sra.all1.annotation.tsv.zst
+    echo "$f/${sample}!${study}!${ref}!${src}.all2.annotation.tsv.zst" >> additional_gene_sums.files
+    echo "$f/${sample}!${study}!${ref}!${src}.all1.annotation.tsv.zst" >> additional_exon_sums.files
+done
+    
+###Split Gene Sums:
+#setup output file headers
+output_file0=$(echo "$output_file" | sed 's#.gz$##')
+echo "##annotation=$annotation" > $output_file0
+date=$(date "+%Y%m%d %H:%m:%S")
+#echo "##date.generated=2023-04-21 17:12:12.436322" >> $output_file0
+echo "##date.generated=$date" >> $output_file0
+sample_header="gene_id"
+outputs=""
+echo -n "" > additional_gene_sums.jobs
+for f in `cat additional_gene_sums.files`; do
+    sample=$(basename $f)
+    sample=$(echo "$sample" | cut -d'!' -f 1)
+    echo "/usr/bin/time -v /bin/bash -x $sdir/add_additional_gene_sums_per_sample.sh $gene_idsF $f additional_gene_sums/${sample}.summed > additional_sums_runs/${sample}.run 2>&1" >> additional_gene_sums.jobs
+    sample_header="${sample_header}	${sample}"
+    outputs="$outputs $additional_gene_sums/${sample}.summed"
+done
+echo "$sample_header" >> $output_file0
+mkdir -p additional_gene_sums
+mkdir -p additional_sums_runs
+/usr/bin/time -v parallel -j${threads} < additional_gene_sums.jobs > additional_gene_sums.jobs.run${threads} 2>&1
+#now paste the results together
+paste $gene_idsF $outputs >> $output_file0
+pigz --fast -p8 $output_file0
+
+
+###Full exon sums (no need to do summing, just pasting):
+output_file0=$(echo "$output_file" | sed 's#.gz$##' | sed 's#gene#exon#g')
+echo "##annotation=$annotation" > $output_file0
+echo "##date.generated=$date" >> $output_file0
+sample_header="chromosome|start_1base|end_1base|strand"
+outputs=""
+echo -n "" > exon_uzstd.jobs
+for f in `cat additional_exon_sums.files`; do
+    sample=$(basename $f)
+    sample=$(echo "$sample" | cut -d'!' -f 1)
+    echo "/usr/bin/time -v zstd -d $f > additional_sums_runs/${sample}.exon.run 2>&1" >> exon_uzstd.jobs 
+    sample_header="${sample_header}	${sample}"
+    f0=$(echo "$f" | sed 's#.zst$##')
+    outputs="$outputs $f0"
+done
+echo "$sample_header" >> $output_file0
+/usr/bin/time -v parallel -j${threads} < exon_uzstd.jobs > exon_uzstd.jobs.run${threads} 2>&1
+paste $exon_idsF $outputs >> $output_file0
+pigz --fast -p8 $output_file0
---
+++ merge/add_additional_gene_sums_per_sample.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -exo pipefail
+#sum the bamcount (or megadepth) quantification of an additional split (non-overlapping) exon annotation (e.g. gencode v43)
+#per-sample, at the gene level (summing split exons into whole gene sums)
+#assumes both input files are the same order (so they gene_ids line up with the split-exon sums)
+
+#should be list of gene_id's matching # and order of additionall split-exon annotation BED used in Pump pipeline (in bamcount/megadepth), no header, single column (line can have multiple gene_ids separated by a "," for a single split exon)
+#e.g. /shared-data/research/genomics/datasets/recount3/prep4refresh/annotation/human/split_exonsv43.bed.gene_ids
+gene_idsF=$1
+#zst'd compressed file of 4 columns (chr,start,end,bp_sum), no header
+#e.g. DRR001175!DRP000425!hg38!sra.all1.annotation.tsv.zst
+split_exons_sumsF=$2
+#path and name of output file
+output_file=$3
+
+/usr/bin/time -v paste $gene_idsF <(zstd -cd $split_exons_sumsF | cut -f 4) | perl -ne 'chomp; $f=$_; ($g,$b)=split(/\t/,$f,-1); @g=split(/,/,$g,-1); for $g0 (@g) { $h{$g0}+=$b; } END { for $g (sort { $a cmp $b } keys %h) { print "$g\t".$h{$g}."\n"; }}' > $output_file
