98c62d679258f186d2041161537a2ffbfba8097a
btrx-cwilks <chris.wilks@neumoratx.com>
2024-07-29 15:13:20 +0000

    change disk pattern, ensure unified local disk is clean, dont chown

--- check_and_create_fs_for_ephemeral_disks.sh
+++ check_and_create_fs_for_ephemeral_disks.sh
@@ -4,11 +4,19 @@ set -exo pipefail
 #avoids having to parcel up the available local ssd space
 make1FS=$1
 #do the following: a) skip loop devices b) find device with root partition (e.g. nvme0n1), get its device name
-root=$(lsblk | tail -n+2 | egrep -v -e '^loop' | fgrep " part /" | perl -ne 'chomp; $f=$_; $f=~/^......([^\s]+)p\d/; $n=$1; print "$n\n";')
+#root=$(lsblk | tail -n+2 | egrep -v -e '^loop' | fgrep " part /" | perl -ne 'chomp; $f=$_; $f=~/^......([^\s]+)p\d/; $n=$1; print "$n\n";')
+root=$(lsblk | tail -n+2 | egrep -v -e '^loop' | tr -s " " $'\t' | cut -f 1,2 | egrep -e ':0$' | cut -f 1)
 i=1
 #skip loop and root devices, assume rest are local SSDs, assigned to /work$i where $i is from 1-#_of_SSDs
 drives=""
 num_drives=0
+#ensure we're starting over clean
+if [[ -n $make1FS ]]; then
+    set +eo pipefail
+    umount /work1
+    mdadm --manage --stop /dev/md1
+    set -eo pipefail
+fi
 echo -n "" > local_disks.txt
 for dev in `lsblk | tail -n+2 | egrep -v -e '^loop' | fgrep -v "$root" | tr -s " " $'\t' | cut -f 1`; do
     if [[ -n $make1FS ]]; then
@@ -18,7 +26,7 @@ for dev in `lsblk | tail -n+2 | egrep -v -e '^loop' | fgrep -v "$root" | tr -s "
         mkfs -q -t ext4 /dev/${dev}
         mkdir -p /work${i} 
         mount /dev/${dev} /work${i}/
-        chown -R ubuntu /work${i}
+        #chown -R recount /work${i}
         chmod -R a+rw /work${i}
         echo "/work${i}" >> local_disks.txt
         i=$((i+1))
@@ -32,6 +40,6 @@ if [[ -n $make1FS ]]; then
     mkfs.ext4 -F /dev/md1
     mkdir -p /work1
     mount /dev/md1 /work1
-    chown -R ubuntu /work1
+    #chown -R ubuntu /work1
     chmod -R a+rw /work1
 fi
