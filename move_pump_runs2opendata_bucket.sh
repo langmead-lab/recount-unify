91678a21fdc219a4b4dd7e3558a90b0c161f11c0
btrx-cwilks <chris.wilks@neumoratx.com>
2024-08-07 16:55:28 +0000

    fix typos, include proper request-payer on first s3 cp

--- move_pump_runs2opendata_bucket.sh
+++ move_pump_runs2opendata_bucket.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -exo pipefaila
+set -exo pipefail
 export LOG_DIR=../transfer_logs
 mkdir -p $LOG_DIR
 #assume the AWS Open Data Creds are available and are being used for these operations
@@ -9,7 +9,7 @@ SB="s3://monorail-batch"
 TB="s3://recount-opendata"
 #human or mouse
 org0=$1
-if [[ -z $org0 ]];
+if [[ -z $org0 ]]; then
     org0="human"
 fi
 pSB=$SB/pump-outputs/$org0
@@ -20,12 +20,12 @@ pDONES="s3://monorail-batch/PUMP_DONES"
 #uSB=$SB/unifier-outputs/$org0
 #uTB=$TB/recount3expansion/unifier/$org0
 
-while 1; do
+while true; do
     #remove any previous done files to avoid confusion about what's already been copied
     rm -rf *.DONE
     #aws s3 ls $pDONES | fgrep ".DONE" | tr -s " " $'\t' | cut -f 4 > pdones
     #get latest set of finsihed samples (from pump)
-    aws s3 cp --recursive $pDONES/ ./
+    aws s3 cp --request-payer requester --recursive $pDONES/ ./
     #for each sample (.DONE file):
     for f in `ls *.DONE`; do
         #get the full S3 source path to copy the pump results from
