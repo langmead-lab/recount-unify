85618676e360dd1498ef8e804114c72403a6316b
btrx-cwilks <chris.wilks@neumoratx.com>
2024-08-09 15:14:21 +0000

    simplify pump2unifier monitoring script to just look for finished studies

--- monitor_finished_pump_studies.sh
+++ monitor_finished_pump_studies.sh
@@ -1,86 +1,87 @@
 #!/usr/bin/env bash
 set -exo pipefail
+#assume this has the source bucket credentials (not open data) only
 #0) starts with manifest of all studies + runs
 #in a loop:
 #1) checks for finished studies in S3 against manifest are aren't in a) unifier q already and b) aren't done unifying
 #2) adds studies from 1) into unifier q
 #3) repeat
 export LC_ALL=C
-#export NUM_EXPECTED_FILES_PER_SAMPLE=36
-export NUM_EXPECTED_FILES_PER_SAMPLE=44
-export PUMP_Q="https://sqs.us-east-1.amazonaws.com/315553526860/monorail_batch_pump"
+#in seconds
+SLEEP_TIME=60
 export UNIFIER_Q="https://sqs.us-east-1.amazonaws.com/315553526860/monorail_batch_unify"
 export REGION=$(echo "$UNIFIER_Q" | cut -d'.' -f 2)
-export PUMP_S3_OUTPUT="s3://monorail-batch/pump-outputs"
+#export PUMP_S3_OUTPUT="s3://monorail-batch/pump-outputs"
+export PUMP_S3_OUTPUT="s3://recount-opendata/recount3expansion/pump"
+export UNIFIER_S3_OUTPUT="s3://recount-opendata/recount3expansion/unifier"
 export date=$(date +%Y%m%d_%s)
-
-requeue_samples () {
-    #e.g. ${manifestF}.${date}.notdone.${study}
-    totalF=$1
-    #e.g. ${manifestF}.${date}.notdone.${study}.finished
-    finishedF=$2
-    fgrep -v -f $finishedF $totalF > ${totalF}.samples2requeue
-    #requeue at this point or wait for queue to unhide OR push into DLQ?
-}
 
 #list of all idx<TAB>studies<TAB>runs being processed by Monorail Pump
 manifestF=$1
-#optional, list of studies which are finished pump processing, <TAB>studies<TAB>
-doneF=$2
-
-if [[ -z $doneF ]]; then
-    doneF="STUDIES_QUEUED_FOR_UNIFIER.txt" 
-fi 
-
-wc=0
-if [[ -e $doneF ]]; then
-    wc=$(cat $doneF | wc -l)
+#human or mouse
+org0=$2
+if [[ -z $org0 ]]; then
+    org0="human"
 fi
-if [[ $wc -gt 0 ]]; then
-    fgrep -v -f $doneF $manifestF > ${manifestF}.${date}.notdone
+
+#for fgrepping later
+head -1 $manifestF | tr $'\t' $'\n' | fgrep -n "" > ${manifestF}.fields
+study_col=$(egrep -e $':study_id$' ${manifestF}.fields | cut -d':' -f 1)
+sample_col=$(egrep -e $':external_id$' ${manifestF}.fields | cut -d':' -f 1)
+if [[ $sample_col -lt $study_col ]]; then
+    paste <(cut -f $study_col $manifestF) <(cut -f $sample_col $manifestF) | sed 's#^#\t' | sed 's#$#\t#' > ${manifestF}.cut.tabs
 else
-    ln -fs $manifestF ${manifestF}.${date}.notdone
+    cut -f ${study_col},${sample_col} $manifestF | sed 's#^#\t' | sed 's#$#\t#' > ${manifestF}.cut.tabs
 fi
+mkdir -p PUMP_DONES
+export PUMP_DONES=$PUMP_S3_OUTPUT/$org0/DONES
 
-cut -f 2 ${manifestF}.${date}.notdone | sed 's#^#/#' | sed 's#$#/#' | sort -u > ${manifestF}.${date}.notdone.studies
+while true; do
+    aws s3 sync --no-sign-request $PUMP_DONES/ ./PUMP_DONES/
+    #example of path in the .DONE file:
+    #ERP001942.ERR188246.20240807_1723047815.DONE
+    #containing:
+    #s3://recount-opendata/recount3expansion/pump/human/95/SRP222095/69/SRR10133969.20240807_1723052789
 
-echo -n "" > s3.pump.${date}
-top_levels=$(aws s3 ls $PUMP_S3_OUTPUT/ | tr -s " " $'\t' | cut -f3)
-for d in $top_levels; do
-    aws s3 ls $PUMP_S3_OUTPUT/$d | tr -s " " $'\t' | cut -f 3 | sed 's#^#'$PUMP_S3_OUTPUT'/'$d'#' >> s3.pump.${date}
-done
-fgrep -f ${manifestF}.${date}.notdone.studies s3.pump.${date} > s3.pump.${date}.notdone
+    #determine which studies are finished (ignore unfinished studies for now, another script will take care of stragglers):
+    pushd ./PUMP_DONES/
+    #get study,run ids for the DONE samples
+    ls *.DONE | cut -d'.' -f 1-2 | sort -u > dones
+    #count number of finished run ids for each study
+    cut -f 1 dones | sort | uniq -c | sed 's#^# #' | tr -s " " $'\t' | cut -f 2- | sort > dones.counts2study
+    #format the list of studies for grepping
+    cut -f 2 dones.counts2study | sed 's#$#\t#' | sed 's#^#\t#' > dones.counts2study.tabs 
+    #grep out the list of studies and get the expected run counts for each study to compare with
+    fgrep -f dones.counts2study.tabs ${manifestF}.cut.tabs | cut -f 2-3 | sort | uniq -c | sed 's#^# #' | tr -s " " $'\t' | cut -f 2- | sort > ${manifestF}.cut.tabs.subset_counts
 
-wc=$(cat s3.pump.${date}.notdone | wc -l)
-if [[ $wc -gt 0 ]]; then
-   for study_s3 in `cat s3.pump.${date}.notdone`; do
-        study=$(basename $study_s3 | sed 's#/$##')
-        #-----First: count number of non-0 byte manifest files as a first check on all samples being finished for the study
-        fgrep $'\t'"$study"$'\t' ${manifestF}.${date}.notdone | sed 's#$#\t#' > ${manifestF}.${date}.notdone.${study}
-        num_samples_expected=$(cat ${manifestF}.${date}.notdone.${study} | wc -l)
-        aws s3 ls --recursive $study_s3 | tr -s " " $'\t' | cut -f 1,3,4 > ${manifestF}.${date}.notdone.${study}.inS3
-        cat ${manifestF}.${date}.notdone.${study}.inS3 | fgrep manifest | fgrep -v $'\t0\t' > ${manifestF}.${date}.notdone.${study}.manifestsINs3
-        num_samples_finished=$(cut -f 3 ${manifestF}.${date}.notdone.${study}.manifestsINs3 | cut -d'.' -f 1 | sort -u | wc -l)
-        if [[ $num_samples_expected -ne $num_samples_finished ]]; then
-            #requeue the samples which are missing
-            echo -n "" > ${manifestF}.${date}.notdone.${study}.manifestsINs3.finished
-            for sample in `cut -f 3 ${manifestF}.${date}.notdone.${study}.manifestsINs3 | cut -d'.' -f 1 | sort -u`; do 
-                basename $sample | sed 's#^#\t#' | sed 's#$#\t#' >> ${manifestF}.${date}.notdone.${study}.finished
-            done
-            requeue_samples "${manifestF}.${date}.notdone.${study}.finished" "${manifestF}.${date}.notdone.${study}"
-        else
-            #-----Second: do another check on total number of files since infrequently the manifest count check can miss a true failure
-            cat ${manifestF}.${date}.notdone.${study}.inS3 | cut -f 3 | cut -d'/' -f 1-5 | sort | uniq -c | tr -s " " $'\t' | fgrep $'\t'${NUM_EXPECTED_FILES_PER_SAMPLE}$'\t' | cut -d'.' -f 1 | cut -f 3 | cut -d'/' -f 5 | sed 's#^#\t#' | sed 's#$#\t#' | sort -u > ${manifestF}.${date}.notdone.${study}.finished
-            num_samples_w_expected_num_files=$(cat ${manifestF}.${date}.notdone.${study}.finished | wc -l)
-            if [[ $num_samples_expected -ne $num_samples_w_expected_num_files ]]; then
-                #requeue_samples "${manifestF}.${date}.notdone.${study}.finished" "${manifestF}.${date}.notdone.${study}"
-                echo "NOT_DONE	$study	skipping"
-            else
-                #----Third: all samples done, queue up for Unifier
-                study_s3=$(echo "$study_s3" | sed 's#/$##')
-                aws sqs send-message --region $REGION --queue-url $UNIFIER_Q --message-body "$study_s3"
-                echo $'\t'"$study"$'\t' >> $doneF
-            fi
-        fi
+    #compare finished # of runs per study counts with expected counts, get the studies that match
+    comm -1 -2 ${manifestF}.cut.tabs.counts2study dones.counts2study > dones.counts2study.matching
+    #get just the study ids of the matching ones
+    cut -f 2 dones.counts2study.matching > dones.counts2study.matching.studies
+
+    #now queue up finished studies from pump, for unifying
+    for study0 in `cat dones.counts2study.matching.studies`; do
+        lo=${study0: -2}
+        #send this to the queue
+        studys3="s3://recount-opendata/recount3expansion/pump/$org0/$lo/$study0"
+        echo "$studys3" >> ../ready2unify.studies 
+        #clear pump .DONE files from S3 for this study
+        #the idea being to try to avoid/minimize as best as possible any multiple overlapping enqueues of the same study
+        #(only ever want 1!)
+        for f in `ls ${study0}.*`; do
+            aws s3 rm $PUMP_S3_OUTPUT/$org0/DONES/$f
+            #still track these pump files just in case...
+            aws s3 cp $f $PUMP_S3_OUTPUT/$org0/UNIFYING/
+        done
+        #clear local directory of sample .DONE files for this study as well
+        rm -rf ${study0}.*
+        #finally, when all other sentinel files have been (re)moved, enqueue the unifier
+        aws sqs send-message --region $REGION --queue-url $UNIFIER_Q --message-body "$studys3"
     done
-fi
+    popd
+    sleep $SLEEP_TIME
+done
+
+#mkdir -p UNIFIER_DONES
+#export UNIFIER_DONES=$UNIFIER_S3_OUTPUT/$org0/DONES
+#aws s3 sync --no-sign-request $UNIFIER_DONES/ ./UNIFIER_DONES/
